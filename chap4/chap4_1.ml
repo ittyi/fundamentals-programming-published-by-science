539f507e9c31d33829b4acf7c713ab9717b7fb0a
ittyi <vioiv.k4@gmail.com>
2023-05-26 00:13:43 +0900

    ♻️ 🐛 add: test

--- chap4/chap4_1.ml
+++ chap4/chap4_1.ml
@@ -1,1 +1,8 @@
-let baito_kyuyu years mouth = 850 + (100 * years) * mouth ;;
+let jikyu = 850 ;;
+let baito_kyuyu years mouth = (jikyu + 100 * years) * mouth ;;
+
+let baito_kyuyu_z year month = 
+  month * (jikyu + year * 100) ;;
+
+baito_kyuyu_z 2 100 ;;
+baito_kyuyu 2 100 ;;
