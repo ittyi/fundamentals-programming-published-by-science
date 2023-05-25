16b3cb39f03534c29e890c5bfd806bed7a6429cf
ittyi <vioiv.k4@gmail.com>
2023-05-26 00:25:42 +0900

    add: chap4_2 Model answer

--- chap4/chap4_3.ml
+++ chap4/chap4_3.ml
@@ -1,2 +1,7 @@
 let hyojun_taiju height = height * height * 22 ;;
-hyojun_taiju 100
+
+let hyojun_taiju_z shincho = 
+  shincho *. shincho *. 22.0 ;;
+
+  hyojun_taiju 100;;
+  hyojun_taiju_z 100.;;
