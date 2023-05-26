4e286108b00168dda8e094d4d3b890788fa70354
ittyi <vioiv.k4@gmail.com>
2023-05-26 23:31:56 +0900

    ✨ chap4_5

---
+++ chap4/chap4_5.ml
@@ -0,0 +1,7 @@
+ (* val baito_kyuyo : int -> int -> int = <fun> *)
+ 
+(* val jikoshokai : string -> string = <fun> *)
+ 
+(* val hyojun_taiju : float -> float = <fun> *)
+ 
+(* val bmi : float -> float -> float = <fun> *)
