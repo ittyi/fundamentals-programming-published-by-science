855c8ef76640c6e6d267af2a835257fdf1bc7e84
ittyi <vioiv.k4@gmail.com>
2023-05-27 18:53:10 +0900

    ✨ ex4.7 完成

--- chap4/ex4_7.ml
+++ chap4/ex4_7.ml
@@ -1,6 +1,10 @@
+let crane_legs = 2
+let turtle_legs = 4
+
 (* 目的: 鶴と亀の数から足の数をする *)
 (* tsurukame_no_ashi: int -> int -> int *)
-let tsurukame_no_ashi how_many_birds how_many_turtle = 0
+let tsurukame_no_ashi how_many_birds how_many_turtle = 
+    (how_many_birds * crane_legs) + (how_many_turtle * turtle_legs)
 
 let test_zero = tsurukame_no_ashi 0 0 = 0
 let testcase1 = tsurukame_no_ashi 2 4 = 20
