69f91644bacf42b43592ade5d661c6bde5dd698e
ittyi <vioiv.k4@gmail.com>
2023-05-27 18:06:11 +0900

    ✨ ex4_6: kame_no_ashi完成

--- chap4/ex4_6.ml
+++ chap4/ex4_6.ml
@@ -8,3 +8,15 @@ let tsuru_no_ashi how_many_birds = how_many_birds * crane_legs
 let test_normal1 = tsuru_no_ashi 8 = 16
 let test_zero = tsuru_no_ashi 0 = 0
 let test_normal2 = tsuru_no_ashi 100 = 200
+
+
+let turtle_legs = 4
+
+(* 目的: 亀の数から足の数を算出する *)
+(* kame_no_ashi: int -> int *)
+let kame_no_ashi how_many_turtle = how_many_turtle * turtle_legs
+
+(* test *)
+let test_normal1 = kame_no_ashi 8 = 32
+let test_zero = kame_no_ashi 0 = 0
+let test_normal2 = kame_no_ashi 100 = 400
