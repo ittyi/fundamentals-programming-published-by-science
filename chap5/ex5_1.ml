89f6a64ee9abeeaa928fef95cb36b4dff2f68466
ittyi <vioiv.k4@gmail.com>
2023-05-28 00:56:05 +0900

    ✨ ex5.1: ocamlの条件分岐

---
+++ chap5/ex5_1.ml
@@ -0,0 +1,14 @@
+if 2 < 1 then 3 else 4 ;;
+(* 4 *)
+
+(* if "true" then 3.14 else 2.72 ;; *)
+(* 3.14 -> errerだった。計算結果がtrueかfalseで終わる必要がある *)
+
+if "a" = "b" then false  else true ;;
+(* true *)
+
+(* if true < false then 1 else "2" ;; *)
+(* errer *)
+
+if not (3 = 4) then 1 < 2 else 1 > 2 ;;
+(* true *)
