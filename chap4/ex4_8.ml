8ec91f6363586e1b208593873a87cfb56ec47cb5
ittyi <vioiv.k4@gmail.com>
2023-05-27 19:04:27 +0900

    ✅ add: 4 test case

--- chap4/ex4_8.ml
+++ chap4/ex4_8.ml
@@ -1,3 +1,9 @@
 (* 目的: 鶴亀算 *)
 (* tsurukame: int -> int -> int *)
 let tsurukame how_many how_many_legs = 0
+
+(* test *)
+let test_zero = tsurukame 0 0 = 0
+let test1 = tsurukame 1 2 = 2
+let test2 = tsurukame 2 6 = 2
+let test2 = tsurukame 3 10 = 2
