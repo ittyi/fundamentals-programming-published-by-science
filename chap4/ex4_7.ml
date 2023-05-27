384117c8166b80e49b56b0ecdae909e63db2f7be
ittyi <vioiv.k4@gmail.com>
2023-05-27 18:50:52 +0900

    ✅ add: 3 test case

--- chap4/ex4_7.ml
+++ chap4/ex4_7.ml
@@ -1,3 +1,7 @@
-(* 目的: 鶴亀算をする *)
+(* 目的: 鶴と亀の数から足の数をする *)
 (* tsurukame_no_ashi: int -> int -> int *)
 let tsurukame_no_ashi how_many_birds how_many_turtle = 0
+
+let test_zero = tsurukame_no_ashi 0 0 = 0
+let testcase1 = tsurukame_no_ashi 2 4 = 20
+let testcase2 = tsurukame_no_ashi 10 10 = 60
