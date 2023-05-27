d70d232917aa2575d5e37ce85ab41d7d7ed006a9
ittyi <vioiv.k4@gmail.com>
2023-05-27 18:02:06 +0900

    ✨ ex4_6: tsuru_no_ashi完成

--- chap4/ex4_6.ml
+++ chap4/ex4_6.ml
@@ -1,8 +1,10 @@
+let crane_legs = 2
+
 (* 目的: 鶴の数から足の数を算出する *)
 (* tsuru_no_ashi: int -> int *)
-let tsuru_no_ashi how_many_birds = 0
+let tsuru_no_ashi how_many_birds = how_many_birds * crane_legs
 
 (* test *)
-let test_normal1 = tsuru_no_ashi 2 = 16
+let test_normal1 = tsuru_no_ashi 8 = 16
 let test_zero = tsuru_no_ashi 0 = 0
 let test_normal2 = tsuru_no_ashi 100 = 200
