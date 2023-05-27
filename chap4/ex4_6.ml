b0b191d0f02a105a3b26d96549e2219d62fc5936
ittyi <vioiv.k4@gmail.com>
2023-05-27 17:57:11 +0900

    ✅ add: 3 test case

--- chap4/ex4_6.ml
+++ chap4/ex4_6.ml
@@ -1,3 +1,8 @@
 (* 目的: 鶴の数から足の数を算出する *)
 (* tsuru_no_ashi: int -> int *)
-let tsuru_no_ashi tsuru = 0
+let tsuru_no_ashi ashi = 0
+
+(* test *)
+let test_normal1 = tsuru_no_ashi 2 = 16
+let test_zero = tsuru_no_ashi 0 = 0
+let test_normal2 = tsuru_no_ashi 100 = 200
