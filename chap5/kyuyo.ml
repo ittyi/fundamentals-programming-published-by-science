3e918edc5b8f7b3762bc8153d0090dc23d1b2373
ittyi <vioiv.k4@gmail.com>
2023-05-28 01:08:51 +0900

    ♻️ 式の中でif

--- chap5/kyuyo.ml
+++ chap5/kyuyo.ml
@@ -10,9 +10,10 @@ let yugu_jikyu = 980
 (* 目的: 働いた時間 x に応じたアルバイト代を計算する *)
 (* kyuyo : int -> int *)
 let kyuyo x = 
-  if x < 30 
+  kihonkyu + x * (if x < 30 then jikyu else yugu_jikyu) 
+  (* if x < 30 
     then x * jikyu + kihonkyu
-    else x * yugu_jikyu + kihonkyu
+    else x * yugu_jikyu + kihonkyu *)
 
 (* test *)
 let test1 = kyuyo 25 = 23850
