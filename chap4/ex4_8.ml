0138d0be15cae3feddabb1873e84b86a91328224
ittyi <vioiv.k4@gmail.com>
2023-05-27 19:13:49 +0900

    ✨ ex4_8: 鶴亀算でつるの数を出す完成

--- chap4/ex4_8.ml
+++ chap4/ex4_8.ml
@@ -1,9 +1,11 @@
-(* 目的: 鶴亀算 *)
+(* 目的: 鶴亀算でつるの数を出す *)
 (* tsurukame: int -> int -> int *)
-let tsurukame how_many how_many_legs = 0
+let tsurukame how_many how_many_legs = 
+    how_many - ((how_many_legs - how_many * 2) / 2)
 
 (* test *)
 let test_zero = tsurukame 0 0 = 0
-let test1 = tsurukame 1 2 = 2
-let test2 = tsurukame 2 6 = 2
-let test2 = tsurukame 3 10 = 2
+let test1 = tsurukame 1 2 = 1
+let test2 = tsurukame 2 6 = 1
+let test2 = tsurukame 3 10 = 1
+let test3 = tsurukame 100 274 = 63
