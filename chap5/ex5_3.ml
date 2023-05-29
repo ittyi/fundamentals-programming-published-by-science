3c744ab277e707a9a1693f1353229ac792488615
ittyi <vioiv.k4@gmail.com>
2023-05-29 23:33:49 +0900

    ✅ add: 4 test case

--- chap5/ex5_3.ml
+++ chap5/ex5_3.ml
@@ -1,3 +1,9 @@
 (* 目的：月と日を受け取ってきたら星座を返す *) 
 (* seiza : int -> int -> string *) 
 let seiza tsuki hi = ""
+ 
+(* テスト *) 
+let test1 = seiza 6 11 = "双子座" 
+let test2 = seiza 6 30 = "蟹座" 
+let test3 = seiza 9 17 = "乙女座" 
+let test4 = seiza 10 7 = "天秤座" 
