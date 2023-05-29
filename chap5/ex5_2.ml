be2ad1af7af7b09ab6cfa0866b0ec216ba8b10a9
ittyi <vioiv.k4@gmail.com>
2023-05-29 23:10:47 +0900

    ✨ 正常ケース完成

--- chap5/ex5_2.ml
+++ chap5/ex5_2.ml
@@ -3,7 +3,10 @@ let afternoon = "午後"
 
 (* 目的: 時間を受け取り、午前午後を返す *)
 (* jikan: int -> string *)
-let jikan h = ""
+let jikan h =
+  if (h <= 11) 
+    then morning
+    else afternoon
 
 (* test *)
 let test1 = jikan 0 = morning
