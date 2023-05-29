9d07181f3d848b31d1f70b30614a7f2fa8ef8825
ittyi <vioiv.k4@gmail.com>
2023-05-29 22:59:23 +0900

    ✅ add: 4 test case

--- chap5/ex5_2.ml
+++ chap5/ex5_2.ml
@@ -1,3 +1,9 @@
 (* 目的: 時間を受け取り、午前午後を返す *)
 (* jikan: int -> string *)
 let jikan h = ""
+
+(* test *)
+let test1 = jikan 0 = "午前"
+let test2 = jikan 11 = "午前"
+let test3 = jikan 12 = "午後"
+let test4 = jikan 23 = "午後"
