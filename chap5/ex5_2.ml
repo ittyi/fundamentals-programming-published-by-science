39a15d9d394bec93d217591c135cd0694502dc3b
ittyi <vioiv.k4@gmail.com>
2023-05-29 23:04:41 +0900

    ♻️ 一部共通化

--- chap5/ex5_2.ml
+++ chap5/ex5_2.ml
@@ -1,9 +1,12 @@
+let morning = "午前"
+let afternoon = "午後"
+
 (* 目的: 時間を受け取り、午前午後を返す *)
 (* jikan: int -> string *)
 let jikan h = ""
 
 (* test *)
-let test1 = jikan 0 = "午前"
-let test2 = jikan 11 = "午前"
-let test3 = jikan 12 = "午後"
-let test4 = jikan 23 = "午後"
+let test1 = jikan 0 = morning
+let test2 = jikan 11 = morning
+let test3 = jikan 12 = afternoon
+let test4 = jikan 23 = afternoon
