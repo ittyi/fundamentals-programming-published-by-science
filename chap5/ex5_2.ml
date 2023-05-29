00df757bbdbf11d9d8e441cb54f30d39ab9b2f2e
ittyi <vioiv.k4@gmail.com>
2023-05-29 23:14:30 +0900

    ✅ ♻️ add: 簡単な例外処理追加

--- chap5/ex5_2.ml
+++ chap5/ex5_2.ml
@@ -1,15 +1,20 @@
 let morning = "午前"
 let afternoon = "午後"
+let out_of_range_errer = "errer: Out of range"
 
 (* 目的: 時間を受け取り、午前午後を返す *)
 (* jikan: int -> string *)
 let jikan h =
-  if (h <= 11) 
-    then morning
-    else afternoon
+  if 0 <= h && h <= 23
+    then if (h <= 11) 
+      then morning
+      else afternoon
+    else out_of_range_errer
 
 (* test *)
 let test1 = jikan 0 = morning
 let test2 = jikan 11 = morning
 let test3 = jikan 12 = afternoon
 let test4 = jikan 23 = afternoon
+let test5 = jikan 24 = out_of_range_errer
+let test1 = jikan (-1) = out_of_range_errer
