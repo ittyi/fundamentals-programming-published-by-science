c8b1030afb28c117c6bb68838a8010373f55ec3d
ittyi <vioiv.k4@gmail.com>
2023-06-03 00:01:39 +0900

    ✅ add: kaiteki関数 test case 5つ

--- chap5/kion.ml
+++ chap5/kion.ml
@@ -14,3 +14,10 @@ let test5 = kion 28 = "普通"
 (* 目的: 現在の気温 t が 15 以上 25 以下かどうかをチェックする *)
 (* kaiteki : int -> bool *)
 let kaiteki t = false
+
+(* test: #use "kion.ml";; *)
+let test6 = kaiteki 7 = false
+let test7 = kaiteki 15 = true
+let test8 = kaiteki 20 = true
+let test9 = kaiteki 25 = true
+let test10 = kaiteki 28 = false
