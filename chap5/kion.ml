fd111ee4e1172c0c3965ae72aa5d2ee83db80e3d
ittyi <vioiv.k4@gmail.com>
2023-06-03 00:03:01 +0900

    ♻️ refactor: kion関数にkaiteki 関数を利用する

--- chap5/kion.ml
+++ chap5/kion.ml
@@ -1,7 +1,7 @@
 (* 目的: 現在の気温 t から快適度を表す文字列を計算する *)
 (* kion : int -> string *)
 let kion t = 
-  if 15 <= t && t <= 25 then "快適"
+  if kaiteki t then "快適"
   else "普通"
 
 (* test: #use "kion.ml";; *)
@@ -13,7 +13,8 @@ let test5 = kion 28 = "普通"
 
 (* 目的: 現在の気温 t が 15 以上 25 以下かどうかをチェックする *)
 (* kaiteki : int -> bool *)
-let kaiteki t = false
+let kaiteki t = 
+  15 <= t && t <= 25
 
 (* test: #use "kion.ml";; *)
 let test6 = kaiteki 7 = false
