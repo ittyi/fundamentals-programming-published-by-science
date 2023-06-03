663981a65791a51f822e7850a96eef53004d3d16
ittyi <vioiv.k4@gmail.com>
2023-06-04 00:22:48 +0900

    ✅ add: test case

--- chap5/ex5_7.ml
+++ chap5/ex5_7.ml
@@ -1,3 +1,9 @@
 (* 目的: bmiの数値によって体型を返す *)
 (* taikei : float -> float  -> float *)
-let taikei height weight = 0.
+let taikei height weight = ""
+
+(* test: #use "ex5_7.ml";; *)
+let test1 = taikei 1.7 50 = "やせ"
+let test1 = taikei 1.7 60 = "標準"
+let test1 = taikei 1.7 80 = "肥満"
+let test1 = taikei 1.7 90 = "高度肥満"
