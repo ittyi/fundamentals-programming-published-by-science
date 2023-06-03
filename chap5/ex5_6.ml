c913fdae542d9f6a6f98e9500534474533c7a8f0
ittyi <vioiv.k4@gmail.com>
2023-06-04 00:13:16 +0900

    ✅ add: test case&関数hanbetsushiki

--- chap5/ex5_6.ml
+++ chap5/ex5_6.ml
@@ -1,3 +1,7 @@
+(* hanbetsushiki : float -> float -> float -> float *)
+let hanbetsushiki a b c = 
+  (b *. b) -. 4. *. a *. c 
+
 (* 目的: 二次方程式の係数を与えられたら、虚数解を持つか判別する *)
 (* 虚数解 判別の仕方 *)
 (* ・D＞0のとき　異なる２つの実数解をもつ
@@ -6,3 +10,7 @@
 (* kyosuukai: float -> float -> float -> bool *)
 let kyosuukai a b c = false
 
+(* test: #use "ex5_6.ml";; *)
+let test1 = kyosuukai 2. 3. (-1.) = false
+let test2 = kyosuukai 1. 4. 4. = false
+let test3 = kyosuukai 1. 2. 2. = true
