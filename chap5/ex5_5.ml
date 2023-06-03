7d3463e07d2690268aa1ff8ba889d33b6b0f9b36
ittyi <vioiv.k4@gmail.com>
2023-06-03 23:46:22 +0900

    ✨ 関数 kai_no_kosuu 完成

--- chap5/ex5_5.ml
+++ chap5/ex5_5.ml
@@ -1,13 +1,21 @@
-(* 目的: 二次方程式の係数を与えられたら、解の個数を返す *)
-(* kai_no_kosuu : int -> int -> int -> int *)
-let kai_no_kosuu a b c = 0
+(* hanbetsushiki : int -> int -> int -> int *)
+let hanbetsushiki a b c = 
+  (b *. b) -. 4. *. a *. c 
 
+(* 目的: 二次方程式の係数を与えられたら、解の個数を返す *)
 (* 解の個数とは *)
 (* b ** 2 − 4 * a * c < 0 の時は会の個数は0 *)
 (* b ** 2 − 4 * a * c = 0 の時は会の個数は1 *)
 (* b ** 2 − 4 * a * c < 0 の時は会の個数は2 *)
+(* kai_no_kosuu : int -> int -> int -> int *)
+let kai_no_kosuu a b c = 
+  if hanbetsushiki a b c < 0. then 0
+  else if hanbetsushiki a b c = 0. then 1
+  else 2
+
 
 (* test: #use "ex5_5.ml";; *)
 let test1 = kai_no_kosuu 2. 3. (-1.) = 2
 let test2 = kai_no_kosuu 1. 4. 4. = 1
-let test3 = kai_no_kosuu 1. 2. 1. = 0
+let test3 = kai_no_kosuu 1. 2. 2. = 0
+
