7085de2a48734b5f6c4c0fa49d6f9d8d4977d8a4
ittyi <vioiv.k4@gmail.com>
2023-06-03 23:40:53 +0900

    ✅ add: ex5.5 test追加

--- chap5/ex5_5.ml
+++ chap5/ex5_5.ml
@@ -6,3 +6,8 @@ let kai_no_kosuu a b c = 0
 (* b ** 2 − 4 * a * c < 0 の時は会の個数は0 *)
 (* b ** 2 − 4 * a * c = 0 の時は会の個数は1 *)
 (* b ** 2 − 4 * a * c < 0 の時は会の個数は2 *)
+
+(* test: #use "ex5_5.ml";; *)
+let test1 = kai_no_kosuu 2. 3. (-1.) = 2
+let test2 = kai_no_kosuu 1. 4. 4. = 1
+let test3 = kai_no_kosuu 1. 2. 1. = 0
