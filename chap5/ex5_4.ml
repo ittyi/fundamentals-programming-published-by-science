a3b1d7472ecbf158a21afd63257ae28f91932041
ittyi <vioiv.k4@gmail.com>
2023-06-03 22:42:15 +0900

    ✨ ex5.4  関数hanbetsushiki 完成

--- chap5/ex5_4.ml
+++ chap5/ex5_4.ml
@@ -1,9 +1,9 @@
 (* 目的: 二次方程式の判別式を返す関数 *)
-(* hanbetsushiki : int -> int -> int -> int *)
-let hanbetsushiki a b c = 0
-
 (* 判別式とは *)
 (* 二次の多項式 a * x ** 2 + b * x + cに対して， b ** 2 − 4 * a * c のこと *)
+(* hanbetsushiki : int -> int -> int -> int *)
+let hanbetsushiki a b c = 
+  (b * b) - 4 * a * c 
 
 (* test: #use "ex5_4.ml";; *)
 let test1 = hanbetsushiki 2 3 (-1) = 17
