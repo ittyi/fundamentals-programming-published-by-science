061e04eda0ff2890bef93fc30560312983ca9ee9
ittyi <vioiv.k4@gmail.com>
2023-06-04 23:15:07 +0900

    🩹 マッチしないであろうパターンでやるとどうなるのかを確かめた

--- chap7/chap7_2.ml
+++ chap7/chap7_2.ml
@@ -15,3 +15,14 @@ match (3, 5) with
 (* 結果
   - : int = 11
 *)
+
+match (3, 5) with
+(a, b, b) -> a + a + b ;;
+(* 結果
+Error: This pattern matches values of type 'a * 'b * 'c
+       but a pattern was expected which matches values of type int * int
+
+訳: 
+エラー: このパターンは、タイプ 'a * 'b * 'c の値と一致します
+       しかし、型 int * int の値に一致するパターンが予期されていました
+*)
