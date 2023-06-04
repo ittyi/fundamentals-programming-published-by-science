80075e4ffe843eef3ab0d94c51f0dc1a53252473
ittyi <vioiv.k4@gmail.com>
2023-06-04 22:46:22 +0900

    ✨ 関数goukei_to_heikin 完成

--- chap7/ex7_1.ml
+++ chap7/ex7_1.ml
@@ -1,7 +1,10 @@
+(* 合計を求める *)
+let helper_goukei_to_heikin sum = (sum, sum / 5)
+
 (* 目的: 国数理社英の点数を与えたら、その合計点と平均点を組にして返す *)
 (* goukei_to_heikin: int -> int -> int -> int -> int -> (int * int) *)
 let goukei_to_heikin national_language math science society english =
-  (0, 0)
+  helper_goukei_to_heikin (national_language + math + science + society + english)
 
 (* test: #use "ex7_1.ml";; *)
 let test1 = goukei_to_heikin 10 10 10 10 10 = (50, 10)
