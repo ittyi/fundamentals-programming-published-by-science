ba3c68d6f239cfbbd45d5e564ee88820dc8bacbe
ittyi <vioiv.k4@gmail.com>
2023-06-05 00:05:24 +0900

    ✨ 関数seiseki 完成

--- chap7/ex7_2.ml
+++ chap7/ex7_2.ml
@@ -1,6 +1,6 @@
 (* 目的: 名前と成績の組を受け取ったら、○○さんの評価は△△ですという文字列を返す *)
 (* seiseki: string * string -> string *)
-let seiseki (name, result) = ""
+let seiseki (name, result) = name ^ "さんの評価は" ^ result ^ "です"
 
 (* test: #use "ex7_2.ml";; *)
 let test1 = seiseki ("ittyi", "普通") = "ittyiさんの評価は普通です"
