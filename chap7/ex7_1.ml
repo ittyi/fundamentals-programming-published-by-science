2ed672c30024c575e5de6224a70b290271f81837
ittyi <vioiv.k4@gmail.com>
2023-06-04 22:40:21 +0900

    📝 add:  goukei_to_heikinの目的と型定義を記載

---
+++ chap7/ex7_1.ml
@@ -0,0 +1,5 @@
+(* 目的: 国数理社英の点数を与えたら、その合計点と平均点を組にして返す *)
+(* goukei_to_heikin: int -> int -> int -> int -> int -> (int * int) *)
+let goukei_to_heikin national_language math science society english =
+  (0, 0)
+  
