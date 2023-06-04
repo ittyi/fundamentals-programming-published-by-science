cc34d67897fb2ece3ec1390b54741e95897f4cd9
ittyi <vioiv.k4@gmail.com>
2023-06-05 00:04:54 +0900

    ✅ add: test  case

--- chap7/ex7_2.ml
+++ chap7/ex7_2.ml
@@ -1,3 +1,6 @@
 (* 目的: 名前と成績の組を受け取ったら、○○さんの評価は△△ですという文字列を返す *)
 (* seiseki: string * string -> string *)
 let seiseki (name, result) = ""
+
+(* test: #use "ex7_2.ml";; *)
+let test1 = seiseki ("ittyi", "普通") = "ittyiさんの評価は普通です"
