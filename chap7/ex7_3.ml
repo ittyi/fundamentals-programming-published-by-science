113cddc57ecc933f2863b0fe8aa0b887aa4af9a0
ittyi <vioiv.k4@gmail.com>
2023-06-06 23:18:15 +0900

    ♻️ ex7.4 add: テンプレート

--- chap7/ex7_3.ml
+++ chap7/ex7_3.ml
@@ -1,6 +1,7 @@
 (* 目的: x, yの座標を受け取り、x について対象な座標を返す *)
 (* taisho_z : int * int -> int * int *)
-let taisho_z coordinate = (0, 0)
+let taisho_z coordinate = match coordinate with 
+  (x, y) -> (0, 0)
 
 (* test: #use "ex7_3.ml";; *)
 let test1 = taisho_z (0, 0) = (0, 0)
