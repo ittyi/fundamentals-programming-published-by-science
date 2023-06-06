ab137262dfd928f836270a6a54eb1bffb7c85e69
ittyi <vioiv.k4@gmail.com>
2023-06-06 23:29:36 +0900

    ♻️ ex7.4: テンプレートの追加

--- chap7/ex7_4.ml
+++ chap7/ex7_4.ml
@@ -1,7 +1,8 @@
 (* 目的: x, yの平面座標を二つ受け取り、その中点を返す *)
 (* chuten : (int * int) * (int * int) -> int * int*)
 (* 解き方: (((x + x) / 2), ((y + y) / 2)) *)
-let chuten plane_coordinates = 0
+let chuten plane_coordinates = match plane_coordinates with
+| ((x1, y1), (x2, y2)) -> (0, 0)
 
 (* test: #use "ex7_4.ml";; *)
 let test1 = chuten ((2, 3), (4, 1)) = (3, 2)
