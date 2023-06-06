5fbab000122f3fc8543c676fc9f70cbaf0b73cd6
ittyi <vioiv.k4@gmail.com>
2023-06-06 23:28:00 +0900

    ✅ ex7.4 add: 3 test case

--- chap7/ex7_4.ml
+++ chap7/ex7_4.ml
@@ -1,3 +1,9 @@
 (* 目的: x, yの平面座標を二つ受け取り、その中点を返す *)
 (* chuten : (int * int) * (int * int) -> int * int*)
+(* 解き方: (((x + x) / 2), ((y + y) / 2)) *)
 let chuten plane_coordinates = 0
+
+(* test: #use "ex7_4.ml";; *)
+let test1 = chuten ((2, 3), (4, 1)) = (3, 2)
+let test1 = chuten ((2, 1), (6, 3)) = (4, 2)
+let test1 = chuten ((3, 6), (-3, -2)) = (0, 2)
