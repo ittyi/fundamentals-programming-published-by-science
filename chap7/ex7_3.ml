d780907d34ffb46f50a3469ec46fe7c605d4a2f5
ittyi <vioiv.k4@gmail.com>
2023-06-06 23:16:46 +0900

    ✅ ex7.3 add: 3 test case

--- chap7/ex7_3.ml
+++ chap7/ex7_3.ml
@@ -1,3 +1,8 @@
 (* 目的: x, yの座標を受け取り、x について対象な座標を返す *)
-(* taisho_z : int * int -> int *)
-let taisho_z coordinate = 0
+(* taisho_z : int * int -> int * int *)
+let taisho_z coordinate = (0, 0)
+
+(* test: #use "ex7_3.ml";; *)
+let test1 = taisho_z (0, 0) = (0, 0)
+let test2 = taisho_z (2, -1) = (2, 1)
+let test3 = taisho_z (2, 10) = (2, -10)
