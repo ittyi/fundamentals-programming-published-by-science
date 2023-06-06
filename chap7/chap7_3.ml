6b15e7238867a4983c337174b10929626e00f6ba
ittyi <vioiv.k4@gmail.com>
2023-06-06 22:50:08 +0900

    📝 chap7.3 add: 関数addの目的と型定義

---
+++ chap7/chap7_3.ml
@@ -0,0 +1,8 @@
+(* 目的: 二つの整数の組 pair を受け取りその要素の和を返す *)
+(* add: int * int -> inr *)
+let add pair = 0
+
+(* test *)
+let test1 = add (0, 0) = 0
+let test2 = add (3, 5) = 8
+let test3 = add (3, -5) = -2
