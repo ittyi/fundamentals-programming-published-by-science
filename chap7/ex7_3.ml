d3e4da2e07b9e86a9c4e75947ea40cd672d30c3f
ittyi <vioiv.k4@gmail.com>
2023-06-06 23:18:53 +0900

    ✨ ex7.4: 関数taisho_z 完成

--- chap7/ex7_3.ml
+++ chap7/ex7_3.ml
@@ -1,7 +1,7 @@
 (* 目的: x, yの座標を受け取り、x について対象な座標を返す *)
 (* taisho_z : int * int -> int * int *)
 let taisho_z coordinate = match coordinate with 
-  (x, y) -> (0, 0)
+  (x, y) -> (x, y * (-1))
 
 (* test: #use "ex7_3.ml";; *)
 let test1 = taisho_z (0, 0) = (0, 0)
