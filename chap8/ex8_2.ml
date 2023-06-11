4f8cc7b79850a60ad5bd94acf6aad8dd0bdfc99c
ittyi <vioiv.k4@gmail.com>
2023-06-11 23:56:13 +0900

    ✨ ex8.2: レコードの定義と宣言、一部ファイル名変更

---
+++ chap8/ex8_2.ml
@@ -0,0 +1,34 @@
+type okozukai_t = {
+  name: string;
+  price: int;
+  place_of_purchase: string;
+  purchase_date: string;
+}
+
+(* let test = {
+  name: "string";
+  price: "int";
+  place_of_purchase: "string";
+  purchase_date: "string";
+}
+
+おお〜ちゃんと上記の変数宣言だと、こんな感じで弾いてくれたー
+# #use "ex8_2.ml";;
+type okozukai_t = {
+  name : string;
+  price : int;
+  place_of_purchase : string;
+  purchase_date : string;
+}
+File "ex8_2.ml", line 24, characters 10-15:
+24 |   price = "100";
+               ^^^^^
+Error: This expression has type string but an expression was expected of type
+         int*)
+
+let test = {
+  name = "string";
+  price = "100";
+  place_of_purchase = "string";
+  purchase_date = "string";
+}
