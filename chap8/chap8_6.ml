e92dd349b9f381320e06fc9dddabe8ef6a20111e
ittyi <vioiv.k4@gmail.com>
2023-06-13 01:32:32 +0900

    ✅ chap8.6 add: testcase 7つ

--- chap8/chap8_6.ml
+++ chap8/chap8_6.ml
@@ -8,3 +8,19 @@ type gakusei_t = {
 (* 目的: 学生のデータ gakusei を受け取り成績のついたデータを返す *)
 (* hyouka : gakusei_t -> gakusei_t *)
 let hyouka gakusei = {namae = ""; tensuu = 0; seiseki = ""}
+
+(* test: #use "chap8_6.ml";; *)
+let test1 = hyouka {namae = "asai"; tensuu = 90; seiseki = ""}
+                  = {namae = "asai"; tensuu = 90; seiseki = "A"}
+let test2 = hyouka {namae = "asai"; tensuu = 80; seiseki = ""}
+                  = {namae = "asai"; tensuu = 80; seiseki = "A"}
+let test3 = hyouka {namae = "asai"; tensuu = 75; seiseki = ""}
+                  = {namae = "asai"; tensuu = 75; seiseki = "B"}
+let test4 = hyouka {namae = "asai"; tensuu = 70; seiseki = ""}
+                  = {namae = "asai"; tensuu = 70; seiseki = "B"}
+let test5 = hyouka {namae = "asai"; tensuu = 65; seiseki = ""}
+                  = {namae = "asai"; tensuu = 65; seiseki = "C"}
+let test6 = hyouka {namae = "asai"; tensuu = 60; seiseki = ""}
+                  = {namae = "asai"; tensuu = 60; seiseki = "C"}
+let test7 = hyouka {namae = "asai"; tensuu = 55; seiseki = ""}
+                  = {namae = "asai"; tensuu = 55; seiseki = "D"}
