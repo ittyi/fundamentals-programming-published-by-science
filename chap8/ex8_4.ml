b672672b63e56c0c988a17382747883acd3d9658
ittyi <vioiv.k4@gmail.com>
2023-06-14 04:47:31 +0900

    🎨 レコードと関数の定義

---
+++ chap8/ex8_4.ml
@@ -0,0 +1,16 @@
+(* #use "ex8_4.ml";; *)
+type parson_t = {
+  name: string;
+  height: int;
+  body_weight: int;
+  birthday: string;
+  blood_type: string;
+}
+
+let ketsueki_hyouji person_info = {
+  name = "";
+  height = 0;
+  body_weight = 0;
+  birthday = "";
+  blood_type = "";
+}
