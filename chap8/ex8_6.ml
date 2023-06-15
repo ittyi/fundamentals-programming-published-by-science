d6b9701b7eaf4be4c9aa15b2c0beaeef897c0bf5
ittyi <vioiv.k4@gmail.com>
2023-06-16 01:33:35 +0900

    📝 ex8.6: テンプレートの追加

--- chap8/ex8_6.ml
+++ chap8/ex8_6.ml
@@ -7,7 +7,8 @@ type ekimei_t = {
 
 (* 目的: 「路線名、駅名(かな)」の形式で文字列を返す *)
 (* hyoji : ekimei_t -> string *)
-let hyoji ekimei = ""
+let hyoji ekimei = match ekimei with
+| {kanji = kanji ; kana = kana; romaji = r; shozoku = s;} -> ""
 
 (* test: #use "ex8_6.ml";; *)
 let test1 = hyoji {kanji = "茗荷谷"; kana = "みょうがだに"; romaji = "myogadani"; shozoku = "丸の内線";}
