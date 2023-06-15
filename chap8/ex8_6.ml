625a31067bb82835cd374008aa064f00307b5b50
ittyi <vioiv.k4@gmail.com>
2023-06-16 01:35:59 +0900

    ✨ ex8.6: 関数hyoji 完成！一部微修正

--- chap8/ex8_6.ml
+++ chap8/ex8_6.ml
@@ -8,8 +8,8 @@ type ekimei_t = {
 (* 目的: 「路線名、駅名(かな)」の形式で文字列を返す *)
 (* hyoji : ekimei_t -> string *)
 let hyoji ekimei = match ekimei with
-| {kanji = kanji ; kana = kana; romaji = r; shozoku = s;} -> ""
+| {kanji = kanji ; kana = kana; romaji = r; shozoku = s;} -> s ^ ", " ^ kanji ^ "（" ^ kana ^ "）"
 
 (* test: #use "ex8_6.ml";; *)
 let test1 = hyoji {kanji = "茗荷谷"; kana = "みょうがだに"; romaji = "myogadani"; shozoku = "丸の内線";}
-  = "丸の内線, 茗荷谷"
+  = "丸の内線, 茗荷谷（みょうがだに）"
