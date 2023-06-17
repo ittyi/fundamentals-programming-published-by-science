1a8ae61a39bf86f1b6603fe9528429979f41c13c
ittyi <vioiv.k4@gmail.com>
2023-06-18 01:47:17 +0900

    ✨ chap9.3: firstの挙動、restの挙動を少し知った

--- chap9/chap9_3.ml
+++ chap9/chap9_3.ml
@@ -6,3 +6,17 @@ match [] with
 match [] with
 [] -> 0
 | f :: r -> f ;;
+
+match [1; 2; 3] with
+[] -> 0
+| first :: rest -> first ;;
+
+(* match [1; 2; 3] with
+[] -> 0
+| first :: rest -> rest ;;
+File "chap9_3.ml", line 16, characters 19-23:
+16 | | first :: rest -> rest ;;
+                        ^^^^
+Error: This expression has type int list
+       but an expression was expected of type int *)
+
