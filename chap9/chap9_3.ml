38f04b3461de531b6db5c6bae1b4830e8d0e67ac
ittyi <vioiv.k4@gmail.com>
2023-06-18 01:49:36 +0900

    ✨ 渓谷はあるが、2つ目の値を取得する方法を把握

--- chap9/chap9_3.ml
+++ chap9/chap9_3.ml
@@ -20,3 +20,15 @@ File "chap9_3.ml", line 16, characters 19-23:
 Error: This expression has type int list
        but an expression was expected of type int *)
 
+match [1; 2; 3] with
+[] -> 0
+| first :: second :: rest -> second ;;
+File "chap9_3.ml", lines 23-25, characters 0-35:
+(* お、渓谷は出るけど2は取得できた。
+23 | match [1; 2; 3] with
+24 | [] -> 0
+25 | | first :: second :: rest -> second...
+Warning 8 [partial-match]: this pattern-matching is not exhaustive.
+Here is an example of a case that is not matched:
+_::[]
+- : int = 2 *)
