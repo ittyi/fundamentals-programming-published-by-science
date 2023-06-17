c861a33892e6aabff66044467d09036b0571e25b
ittyi <vioiv.k4@gmail.com>
2023-06-18 02:32:50 +0900

    📝 chap9.4: 目的と型定義の追加

--- chap9/chap9_4.ml
+++ chap9/chap9_4.ml
@@ -2,4 +2,6 @@
   - [] 空リスト、あるいは
   - first :: rest 最初の要素が first で残りのリストが rest という形 *)
 
-  
+(* 目的: 受け取ったリスト list に 0 が含まれているかを調べる *)
+(* contain_zero : int list -> bool *)
+let contain_zero lst = false
