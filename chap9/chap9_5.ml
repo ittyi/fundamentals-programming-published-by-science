a64713e7c3be8c9a095bbaf3ed71a678d243c837
ittyi <vioiv.k4@gmail.com>
2023-06-21 02:09:33 +0900

    ♻️ chap9.5: テンプレートの作成

--- chap9/chap9_5.ml
+++ chap9/chap9_5.ml
@@ -6,10 +6,14 @@
 
 (* 目的: 受け取ったリスト lst の各要素の和を求める *)
 (* sum : int list -> int *)
-let sum lst = 0
+let sum lst = match lst with
+| [] -> 0
+| first :: rest -> 0 (* sum rest *)
 
 (* test: #use "chap9_5.ml";; *)
 let test1 = sum [] = 0
 let test2 = sum [2] = 2
 let test3 = sum [1; 3] = 4
 let test4 = sum [1; 2; 3; 4; 5; 6; 7; 8; 9; 10] = 55
+
+(* ポイントとしては、自己参照のケースがあれば、再帰を利用できる。ってところか。 *)
