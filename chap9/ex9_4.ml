c5a5a4e21d14e785396db1491e3805e467033170
ittyi <vioiv.k4@gmail.com>
2023-06-21 02:44:49 +0900

    ✅ ex9.4: add test 3case

--- chap9/ex9_4.ml
+++ chap9/ex9_4.ml
@@ -6,3 +6,8 @@
 (* 目的: リストの長さを返す関数 *)
 (* length : int list -> int *)
 let length lst = 0
+
+(* test: #use "ex9_4.ml";; *)
+let test1 = length [] = 0
+let test2 = length [2; 1; 6; 4; 7] = 5
+let test3 = length [2] = 1
