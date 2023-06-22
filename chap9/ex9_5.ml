964d42b5330116d93d2aefdea1d60e207fd6f122
ittyi <vioiv.k4@gmail.com>
2023-06-22 22:27:23 +0900

    ✅ ex9.5: add test 4case

--- chap9/ex9_5.ml
+++ chap9/ex9_5.ml
@@ -7,4 +7,8 @@
 (* even : int list -> int list *)
 let even lst = lst
 
-(* #use "ex9_5.ml";; *)
+(* test: #use "ex9_5.ml";; *)
+let test1 = even [] = []
+let test2 = even [2; 1; 6; 4; 7] = [2; 6; 4]
+let test3 = even [1; 5; 7] = []
+let test2 = even [2; 6; 4] = [2; 6; 4]
