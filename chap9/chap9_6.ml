bba35d0701b9d501496e19bea3229beb5e8a58b1
ittyi <vioiv.k4@gmail.com>
2023-06-23 05:00:47 +0900

    🎨 & 📝 chap9.6: データの定義と説明を記載

---
+++ chap9/chap9_6.ml
@@ -0,0 +1,16 @@
+(* レコードのリストなど、入力が複雑になった場合のテンプレートについて学ぶ。 *)
+
+(* 学生ひとり分のデータ（名前、点数、成績）を表す型 *) 
+type gakusei_t = { 
+  namae : string;       (* 名前 *) 
+  tensuu : int;         (* 点数 *) 
+  seiseki : string;     (* 成績 *) 
+} 
+ 
+(* gakusei_t list は 
+     - []              空リスト、あるいは 
+     - first :: rest   最初の要素が first で残りのリストが rest 
+                       （first は gakusei_t 型、 
+                         rest が自己参照のケース） 
+   という形 *) 
+
