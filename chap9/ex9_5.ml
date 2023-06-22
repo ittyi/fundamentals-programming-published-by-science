40c2d6db909f36ded2e4c2fbdb796b6624a42080
ittyi <vioiv.k4@gmail.com>
2023-06-22 22:32:01 +0900

    🚧 ex9.5: ロジックをコメントに書いた。+ first step.

--- chap9/ex9_5.ml
+++ chap9/ex9_5.ml
@@ -7,10 +7,14 @@
 (* even : int list -> int list *)
 let even lst = match lst with
 | [] -> []
-| first :: rest -> []
+| first :: rest -> first :: []
 
 (* test: #use "ex9_5.ml";; *)
 let test1 = even [] = []
 let test2 = even [2; 1; 6; 4; 7] = [2; 6; 4]
 let test3 = even [1; 5; 7] = []
 let test2 = even [2; 6; 4] = [2; 6; 4]
+
+(* で、たぶん調べないといけないのが list への追加の仕方。
+step1: まずは list をそのまま返そう。
+step2: 次に、再帰を組み込もう。 *)
