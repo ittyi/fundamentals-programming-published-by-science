03fe78324b9d0e24e895b57a22d90363fef1935d
ittyi <vioiv.k4@gmail.com>
2023-06-23 05:06:35 +0900

    ♻️ chap9.6: テンプレートの作成

--- chap9/chap9_6.ml
+++ chap9/chap9_6.ml
@@ -26,10 +26,12 @@ let lst4 = [{namae = "yoshida"; tensuu = 80; seiseki = "A"};
 
 (* 目的：学生リスト lst のうち成績が A の人の数を返す *) 
 (* count_A : gakusei_t list -> int *) 
-let rec count_A lst = 0
+let rec count_A lst = match lst with
+  [] -> 0
+| first :: rest -> 0
  
-(* テスト *) 
+(* test: #use "chap9_6.ml";; *) 
 let test1 = count_A lst1 = 0 
 let test2 = count_A lst2 = 0 
 let test3 = count_A lst3 = 1 
-let test4 = count_A lst4 = 2 
+let test4 = count_A lst4 = 2
