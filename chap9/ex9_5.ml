80e8cee1ec2a5e5604dd266ce9d126155ea67ad3
ittyi <vioiv.k4@gmail.com>
2023-06-22 22:25:20 +0900

    📝 ex9.5: データ構造、目的、型定義の明記

---
+++ chap9/ex9_5.ml
@@ -0,0 +1,10 @@
+(* データ定義
+   [] : 空リスト
+   first :: rest　-> 最初の要素が first で残りのリストが rest
+    ( rest が自己参照のケース) *)
+
+(* 目的: 整数のリストのうち、偶数の要素をリスト化して返す *)
+(* even : int list -> int list *)
+let even lst = lst
+
+(* #use "ex9_5.ml";; *)
