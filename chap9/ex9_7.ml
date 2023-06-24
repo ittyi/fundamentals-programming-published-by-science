451cad23d84dac12a8a4f87a4a8109144f8a429e
ittyi <vioiv.k4@gmail.com>
2023-06-24 22:33:22 +0900

    📝 ex9.7: 目的と型定義を記載

--- chap9/ex9_7.ml
+++ chap9/ex9_7.ml
@@ -13,3 +13,10 @@ parson_t list は
      - first :: rest   最初の要素が first で残りのリストが rest 
                        （first は parson_t 型、rest が自己参照のケース）    
 *)
+
+(* 目的: 人の情報リストを受け取り、血液型Aの人間をレコードとして返す *)
+(* count_ketsueki_A : parson_t list -> parson_t list *)
+let count_ketsueki_A lst = lst
+(* val count_ketsueki_A : 'a -> 'a = <fun> *)
+
+(* test: #use "ex9_7.ml";; *)
