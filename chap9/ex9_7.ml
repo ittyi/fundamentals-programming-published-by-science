89a887333bccdb8d56b072563fd338852e2680e1
ittyi <vioiv.k4@gmail.com>
2023-06-25 23:16:48 +0900

    ✨ ex9.7: 色々間違ってたけど関数count_ketsueki_A 完成！

--- chap9/ex9_7.ml
+++ chap9/ex9_7.ml
@@ -15,21 +15,21 @@ parson_t list は
 *)
 
 (* 目的: 人の情報リストを受け取り、血液型Aの人間をレコードとして返す *)
-(* count_ketsueki_A : parson_t list -> parson_t list *)
-let count_ketsueki_A lst = match lst with
-| [] -> []
+(* count_ketsueki_A : parson_t list -> int *)
+let rec count_ketsueki_A lst = match lst with
+| [] -> 0
 | {
     name = n;
     height = h;
     body_weight = bw;
     birthday = b;
     blood_type = bt;
-  } :: rest -> []
-  (* then [] (* count_ketsueki_A rest *)
-  else  *)
+  } :: rest -> if bt = "A"
+    then  1 + count_ketsueki_A rest
+    else count_ketsueki_A rest
 
 (* test: #use "ex9_7.ml";; *)
-let test1 = count_ketsueki_A [] = []
+let test1 = count_ketsueki_A [] = 0
 let test2 = count_ketsueki_A [
   {
     name = "Aの人1";
@@ -43,7 +43,7 @@ let test2 = count_ketsueki_A [
     height = 160;
     body_weight = 50;
     birthday = "12/24";
-    blood_type = "A";
+    blood_type = "B";
   };
   {
     name = "Aの人2";
@@ -52,19 +52,4 @@ let test2 = count_ketsueki_A [
     birthday = "12/24";
     blood_type = "A";
   }
-] = [
-  {
-    name = "Aの人1";
-    height = 150;
-    body_weight = 40;
-    birthday = "12/24";
-    blood_type = "A";
-  };
-  {
-    name = "Aの人2";
-    height = 170;
-    body_weight = 60;
-    birthday = "12/24";
-    blood_type = "A";
-  }
-]
+] = 2
