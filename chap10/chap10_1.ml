114464dd1af96b7ce9c3dc6409d8fbb4e035e0d8
ittyi <vioiv.k4@gmail.com>
2023-06-29 03:25:27 +0900

    ✅ chap10.1: prefixのテストを4つ追加

--- chap10/chap10_1.ml
+++ chap10/chap10_1.ml
@@ -5,3 +5,9 @@ let test3 = add_to_each 1 [[2]; [2; 3]] = [[1; 2]; [1; 2; 3]]
 let test4 = add_to_each 1 [[2]; [2; 3]; [2; 3; 4]] 
                           = [[1; 2]; [1; 2; 3]; [1; 2; 3; 4]] 
  
+(* テスト *) 
+let test5 = prefix [] = [] 
+let test6 = prefix [1] = [[1]] 
+let test7 = prefix [1; 2] = [[1]; [1; 2]] 
+let test8 = prefix [1; 2; 3; 4] 
+                   = [[1]; [1; 2]; [1; 2; 3]; [1; 2; 3; 4]] 
