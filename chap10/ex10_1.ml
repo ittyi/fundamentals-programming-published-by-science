6da98490f9690e80392487d1e73f36377a690a33
ittyi <vioiv.k4@gmail.com>
2023-06-30 03:49:32 +0900

    📝 ex10.1: データ定義

--- chap10/ex10_1.ml
+++ chap10/ex10_1.ml
@@ -1,3 +1,11 @@
+(* データ定義 *)
+(* int list は 
+     - []              空リスト
+     - first :: rest   最初の要素が first で残りのリストが rest 
+                       （first は int 型、 
+                         rest が自己参照のケース） 
+*) 
+
 (* 目的: 整数の昇順リストと整数 n を受け取ったら、前から順に見ていき照準を崩さずに n をリストに挿入する関数 *)
 (* insert : int list n -> int list *)
 let insert [] 0 = []
