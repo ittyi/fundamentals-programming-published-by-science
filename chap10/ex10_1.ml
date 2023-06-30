f3bc2a484bf14786dc42d424b57cfc7c503b3800
ittyi <vioiv.k4@gmail.com>
2023-06-30 11:01:26 +0900

    🚧 ex10.1: 作業途中だけど保存

--- chap10/ex10_1.ml
+++ chap10/ex10_1.ml
@@ -10,8 +10,18 @@
 (* insert : int list n -> int list *)
 let insert intList integer = match intList with
 | [] -> [integer]
-| first :: rest -> []
+| first :: rest -> if first > integer 
+    then integer :: first :: rest
+    else first :: (insert rest integer)
 
 (* test: #use "ex10_1.ml";; *)
 let test1 = insert [] 0 = [0]
-let test2 = insert [1; 3; 4; 7; 8] 5 = [1; 3; 4; 5; 7; 8]
+let test2 = insert [1; 3; 4; 7; 8] 5
+let test3 = insert [1] 5
+ (* = [1; 5] *)
+let test4 = insert [5] 1 = [1; 5]
+(* = [1; 3; 4; 5; 7; 8] *)
+
+(* memo
+必ず昇順なのであれば、if文で代償を確認すればできそう   
+*)
