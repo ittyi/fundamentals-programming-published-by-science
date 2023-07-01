b84c6c38c4cc69aa42de87b39ac20ab80942139e
ittyi <vioiv.k4@gmail.com>
2023-07-01 21:05:41 +0900

    ✨ ex10.1: 関数insert 完成

--- chap10/ex10_1.ml
+++ chap10/ex10_1.ml
@@ -8,7 +8,7 @@
 
 (* 目的: 整数の昇順リストと整数 n を受け取ったら、前から順に見ていき照準を崩さずに n をリストに挿入する関数 *)
 (* insert : int list n -> int list *)
-let insert intList integer = match intList with
+let rec insert intList integer = match intList with
 | [] -> [integer]
 | first :: rest -> if first > integer 
     then integer :: first :: rest
@@ -16,11 +16,9 @@ let insert intList integer = match intList with
 
 (* test: #use "ex10_1.ml";; *)
 let test1 = insert [] 0 = [0]
-let test2 = insert [1; 3; 4; 7; 8] 5
-let test3 = insert [1] 5
- (* = [1; 5] *)
-let test4 = insert [5] 1 = [1; 5]
-(* = [1; 3; 4; 5; 7; 8] *)
+let test2 = insert [1; 3; 4; 7; 8] 5 = [1; 3; 4; 5; 7; 8]
+let test3 = insert [1] 5 = [1; 5]
+let test4 = insert [5] 1 = [1; 5] 
 
 (* memo
 必ず昇順なのであれば、if文で代償を確認すればできそう   
