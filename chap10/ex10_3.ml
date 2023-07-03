f97a06a1f37049f3afff7a4c8ab8d7ed6c2363e0
ittyi <vioiv.k4@gmail.com>
2023-07-04 05:10:40 +0900

    ✅ ex10.3: add test

--- chap10/ex10_3.ml
+++ chap10/ex10_3.ml
@@ -5,8 +5,63 @@ type gakusei_t = {
   seiseki: string;  (*成績*)
 }
 
+(* データ定義 *)
+(* int list は 
+     - []              空リスト
+     - first :: rest   最初の要素が first で残りのリストが rest 
+                       （first は int 型、 
+                         rest が自己参照のケース） 
+*) 
+
 (* 目的: gakusei_t 型を受け取り、tensuu フィールドの順に整列する関数 *)
 (* gakusei_sort : gakusei_t list -> gakusei_t list *)
 let gakusei_sort gakusei = []
 
 (* test: #use "ex10_3.ml";; *)
+let test1 = gakusei_sort [
+  {
+    namae = "namae1";    (*名前*)
+    tensuu = 1;      (*点数*)
+    seiseki = "A";  (*成績*)
+  };
+  {
+    namae = "namae2";    (*名前*)
+    tensuu = 2;      (*点数*)
+    seiseki = "B";  (*成績*)
+  };
+] = [
+  {
+    namae = "namae1";    (*名前*)
+    tensuu = 1;      (*点数*)
+    seiseki = "A";  (*成績*)
+  };
+  {
+    namae = "namae2";    (*名前*)
+    tensuu = 2;      (*点数*)
+    seiseki = "B";  (*成績*)
+  };
+] 
+
+let test2 = gakusei_sort [
+  {
+    namae = "namae1";    (*名前*)
+    tensuu = 2;      (*点数*)
+    seiseki = "A";  (*成績*)
+  };
+  {
+    namae = "namae2";    (*名前*)
+    tensuu = 1;      (*点数*)
+    seiseki = "B";  (*成績*)
+  };
+] = [
+  {
+    namae = "namae2";    (*名前*)
+    tensuu = 1;      (*点数*)
+    seiseki = "A";  (*成績*)
+  };
+  {
+    namae = "namae1";    (*名前*)
+    tensuu = 2;      (*点数*)
+    seiseki = "B";  (*成績*)
+  };
+] 
