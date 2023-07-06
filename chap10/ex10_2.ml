ba0c4ca03d9241f7ea874b839cc31d97fbb8c627
ittyi <vioiv.k4@gmail.com>
2023-07-06 23:06:42 +0900

    🩹 ex10.2: タイポ修正

--- chap10/ex10_2.ml
+++ chap10/ex10_2.ml
@@ -1,4 +1,4 @@
-(* 目的: 整数の昇順リストと整数 n を受け取ったら、前から順に見ていき照準を崩さずに n をリストに挿入する関数 *)
+(* 目的: 整数の昇順リストと整数 n を受け取ったら、前から順に見ていき昇順を崩さずに n をリストに挿入する関数 *)
 (* insert : int list n -> int list *)
 let rec insert intList integer = match intList with
 | [] -> [integer]
