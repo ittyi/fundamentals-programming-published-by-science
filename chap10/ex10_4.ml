5b3988de3b3b9cc6c5bfcbb3bdf76e63f729d02c
ittyi <vioiv.k4@gmail.com>
2023-07-09 02:08:07 +0900

    ✅ ex10.4: testデータとてテストの準備

--- chap10/ex10_4.ml
+++ chap10/ex10_4.ml
@@ -16,6 +16,29 @@ type parson_t = {
                          rest が自己参照のケース） 
 *) 
 
+(* parsonデータの例 *) 
+let parson1 = {name = "stringName1"; height = 150; body_weight = 40; birthday = "1/1"; blood_type = "A"} 
+let parson2 = {name = "stringName2"; height = 160; body_weight = 50; birthday = "2/2"; blood_type = "B"} 
+let parson3 = {name = "stringName3"; height = 170; body_weight = 60; birthday = "3/3"; blood_type = "O"} 
+let parson4 = {name = "stringName4"; height = 180; body_weight = 70; birthday = "4/4"; blood_type = "AB"} 
+let parson5 = {name = "stringName5"; height = 190; body_weight = 80; birthday = "5/5"; blood_type = "A"} 
+let parson6 = {name = "stringName6"; height = 200; body_weight = 90; birthday = "6/6"; blood_type = "B"} 
+ 
+(* parsonリストの例 *) 
+let lst1 = [] 
+let lst2 = [parson1] 
+let lst3 = [parson3; parson4] 
+let lst4 = [parson4; parson3] 
+let lst5 = [parson4; parson1; parson6; parson5; parson2; parson3] 
+let lst6 = [parson1; parson2; parson3; parson4; parson5; parson6] 
+
+
 (* 目的: 名前の順で整列 *)
 (* parson_sort : parson_t list -> parson_t list *)
 let parson_sort lst = []
+
+(* test *)
+let test1 = parson_sort [] = []
+let test2 = parson_sort lst2 = lst2
+let test3 = parson_sort lst3 = lst3
+let test3 = parson_sort lst4 = lst3
