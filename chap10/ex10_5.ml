d0f1c44a7e1796a8c5a47f13e8ee5f2c7b79a527
ittyi <vioiv.k4@gmail.com>
2023-07-10 03:06:28 +0900

    ✅ ex10.5: test data、test case の用意

--- chap10/ex10_5.ml
+++ chap10/ex10_5.ml
@@ -1,17 +1,36 @@
 (* 学生1人分のデータ（名前、点数、成績）を表す型 *)
 type gakusei_t = {
-  namae: string;    (*名前*)
+  name: string;    (*名前*)
   tensuu: int;      (*点数*)
   seiseki: string;  (*成績*)
 }
 
+(* gakuseiデータの例 *) 
+let gakusei1 = {name = "gakusei1"; tensuu = 40; seiseki = "A"} 
+let gakusei2 = {name = "gakusei2"; tensuu = 50; seiseki = "B"} 
+let gakusei3 = {name = "gakusei3"; tensuu = 60; seiseki = "C"} 
+let gakusei4 = {name = "gakusei4"; tensuu = 70; seiseki = "D"} 
+let gakusei5 = {name = "gakusei5"; tensuu = 80; seiseki = "E"} 
+let gakusei6 = {name = "gakusei6"; tensuu = 90; seiseki = "A"} 
+ 
+(* gakuseiリストの例 *) 
+let lst1 = [] 
+let lst2 = [gakusei1] 
+let lst3 = [gakusei3; gakusei4] 
+let lst4 = [gakusei4; gakusei1; gakusei5; gakusei2; gakusei3] 
+let lst5 = [gakusei1; gakusei2; gakusei3; gakusei4; gakusei5; gakusei6] 
+
 (* 目的: 最高点数をとった学生のレコードを返す *)
 (* gakusei_max : gakusei_t list -> gakusei_t *)
 let gakusei_max lst = {
-  namae = "name";
+  name = "name";
   tensuu = 0; 
   seiseki = "A";
 }
 
 (* test *)
 (* #use "ex10_5.ml";; *)
+let test1 = gakusei_max lst5 = gakusei6
+let test2 = gakusei_max lst2 = gakusei1
+let test3 = gakusei_max lst3 = gakusei4
+let test4 = gakusei_max lst4 = gakusei5
