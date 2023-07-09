9b76ef30675edbaee65e6c22220f9efd74be279b
ittyi <vioiv.k4@gmail.com>
2023-07-10 02:22:01 +0900

    📝 chap10.2: max_intの動作確認

--- chap10/chap10_2.ml
+++ chap10/chap10_2.ml
@@ -4,8 +4,14 @@ let rec minimum lst = match lst with
     [] -> max_int 
   | first :: rest -> 0 (* minimum rest  *)
  
-(* テスト *) 
+(* test: #use "chap10_2.ml";; *) 
 let test1 = minimum [3] = 3 
 let test2 = minimum [1; 2] = 1 
 let test3 = minimum [3; 2] = 2 
 let test4 = minimum [3; 2; 6; 4; 1; 8] = 1 
+
+(* 動作確認 *)
+let max_int_test = max_int
+(* 
+val max_int_test : int = 4611686018427387903 
+*)
