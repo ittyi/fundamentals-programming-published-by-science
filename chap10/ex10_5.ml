556a2233b7d73f8e0dd956bb60b1e4aac4fb1cd8
ittyi <vioiv.k4@gmail.com>
2023-07-10 03:12:08 +0900

    📝 ex10.5: テンプレートと再帰のデータ定義も追加

--- chap10/ex10_5.ml
+++ chap10/ex10_5.ml
@@ -5,6 +5,13 @@ type gakusei_t = {
   seiseki: string;  (*成績*)
 }
 
+(* gakusei_t list は 
+     - []              空リスト
+     - first :: rest   最初の要素が first で残りのリストが rest 
+                       （first は gakusei_t 型、 
+                         rest が自己参照のケース） 
+*) 
+
 (* gakuseiデータの例 *) 
 let gakusei1 = {name = "gakusei1"; tensuu = 40; seiseki = "A"} 
 let gakusei2 = {name = "gakusei2"; tensuu = 50; seiseki = "B"} 
@@ -22,11 +29,15 @@ let lst5 = [gakusei1; gakusei2; gakusei3; gakusei4; gakusei5; gakusei6]
 
 (* 目的: 最高点数をとった学生のレコードを返す *)
 (* gakusei_max : gakusei_t list -> gakusei_t *)
-let gakusei_max lst = {
+let gakusei_max lst = match lst with
+| [] -> gakusei6
+| first :: rest -> gakusei6
+  
+  (* {
   name = "name";
   tensuu = 0; 
   seiseki = "A";
-}
+} *)
 
 (* test *)
 (* #use "ex10_5.ml";; *)
