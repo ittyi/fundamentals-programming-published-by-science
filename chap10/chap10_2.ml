4a90a6743820759a8668cc4255cc95158bd34729
ittyi <vioiv.k4@gmail.com>
2023-07-10 02:46:58 +0900

    ✅ chap10.2: 念の為testを追加

--- chap10/chap10_2.ml
+++ chap10/chap10_2.ml
@@ -12,6 +12,7 @@ let test1 = minimum [3] = 3
 let test2 = minimum [1; 2] = 1 
 let test3 = minimum [3; 2] = 2 
 let test4 = minimum [3; 2; 6; 4; 1; 8] = 1 
+let test4 = minimum [3; 2; 2; 1; 1; 8] = 1 
 
 (* 動作確認 *)
 let max_int_test = max_int
