879abb9f7a636e828ff9cb80fdd4f15e9984618c
ittyi <vioiv.k4@gmail.com>
2023-07-11 22:50:30 +0900

    📝 chap10.3: chap10.2のコードを引っ張る

---
+++ chap10/chap10_3.ml
@@ -0,0 +1,23 @@
+(* 目的：受け取った lst の中の最小値を返す *) 
+(* minimum : int list -> int *) 
+let rec minimum lst = match lst with 
+    [] -> max_int
+  | first :: rest -> if first < (minimum rest)
+    then first
+    else (minimum rest)
+
+
+(* test: #use "chap10_2.ml";; *) 
+let test1 = minimum [3] = 3 
+let test2 = minimum [1; 2] = 1 
+let test3 = minimum [3; 2] = 2 
+let test4 = minimum [3; 2; 6; 4; 1; 8] = 1 
+let test4 = minimum [3; 2; 2; 1; 1; 8] = 1 
+
+(* 動作確認 *)
+let max_int_test = max_int
+(* 
+val max_int_test : int = 4611686018427387903 
+→ 上記は 64bit の場合の max値
+min_int もある。
+*)
