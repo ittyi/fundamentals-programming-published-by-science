bcdc0cbdc026db4f2b435253798e12b989e92966
ittyi <vioiv.k4@gmail.com>
2023-07-12 00:24:00 +0900

    ✨ chap10.4: 関数shukei 完成？

--- chap10/chap10_4.ml
+++ chap10/chap10_4.ml
@@ -12,12 +12,38 @@ type gakusei_t = {
                          rest が自己参照のケース） 
 *) 
 
+let test_data = {
+  namae = "name1";
+  tensuu = 1;
+  seiseki = "A";
+  }
+
+let test_exc = (0, 0, 0, 0) 
+
 (* 目的: 学生リスト lst のうち各成績の人数を集計する *)
 (* shukei : gakusei_t list -> int * int * int * int *)
-let rec shukei lst = (0),(0), (0), (0)
-  (* match lst with
-| [] -> ()
-| farst :: rest ->  *)
+let rec shukei lst = match lst with
+| [] -> (0, 0, 0, 0)
+| first :: rest -> 
+  match shukei rest with
+  | (a, b, c, d) -> 
+    if first.seiseki = "A" then let a = a + 1 in (a, b, c, d)
+    else if first.seiseki = "B" then let b = b + 1 in (a, b, c, d)
+    else if first.seiseki = "C" then let c = c + 1 in (a, b, c, d)
+    else if first.seiseki = "D" then let d = d + 1 in (a, b, c, d)
+    else (a, b, c, d)
+  (* (first.tensuu, first.tensuu, first.tensuu, first.tensuu) *)
 
 (* test: #use "chap10_4.ml";; *)
-let tset1 = shukei []
+let tset1 = shukei [
+  {
+  namae = "name1";
+  tensuu = 1;
+  seiseki = "A";
+  };
+  {
+    namae = "name2";
+    tensuu = 1;
+    seiseki = "B";
+  };
+]
