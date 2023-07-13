fd4b84e05748918f88b36b7c513f984f11c41059
ittyi <vioiv.k4@gmail.com>
2023-07-14 03:21:00 +0900

    💡 ex10.7: 再帰のデータ構造を明記

--- chap10/ex10_7.ml
+++ chap10/ex10_7.ml
@@ -7,6 +7,13 @@ type parson_t = {
   blood_type: string;
 }
 
+(* parson_t list は 
+     - []              空リスト
+     - first :: rest   最初の要素が first で残りのリストが rest 
+                       （first は parson_t 型、 
+                         rest が自己参照のケース） 
+*) 
+
 (* 目的: 各血液型の人が何人いるかを汲みにして返す *)
 (* ketsueki_shukei : parson_t list => int * int * int * int *)
 let ketsueki_shukei lst = (0, 0, 0, 0)
