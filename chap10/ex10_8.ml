f85a808935cd0712721f180340153eecd55e4483
ittyi <vioiv.k4@gmail.com>
2023-07-16 05:43:56 +0900

    📝 ex10.8: データ定義やデータ構造の追加

--- chap10/ex10_8.ml
+++ chap10/ex10_8.ml
@@ -1,5 +1,21 @@
+type parson_t = {
+  name: string;
+  height: int;
+  body_weight: int;
+  birthday: string;
+  blood_type: string;
+}
+
+(* parson_t list は 
+     - []              空リスト
+     - first :: rest   最初の要素が first で残りのリストが rest 
+                       （first は parson_t 型、 
+                         rest が自己参照のケース） 
+*) 
+
 (* 目的: 最も人数の多い血液型を返す *)
-(* saita_ketsueki : parson_t list -> string *)
-let rec saita_ketsueki lst = 0
+(* saita_ketsueki : parson_t list -> string list *)
+(* 人数が多いとはいえ、同じ人数の時がありそうなので一旦listで返します。 *)
+let rec saita_ketsueki lst = []
 
 (* test: #use "ex10_8.ml";; *)
