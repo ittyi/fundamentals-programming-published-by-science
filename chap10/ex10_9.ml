90937ef34b936216b397b029a2ff04275cd1bcba
ittyi <vioiv.k4@gmail.com>
2023-07-22 03:45:06 +0900

    ✅ ex10.9: 補助関数のテストを追加

--- chap10/ex10_9.ml
+++ chap10/ex10_9.ml
@@ -2,6 +2,9 @@
 (* leng : a7 list -> int *)
 let rec leng lst = 0
 
+let test1 = leng [1, 2, 3] = 3
+let test2 = leng ["a", "b", "c"] = 3
+
 (* 目的: 二つのリストの長さが同じかどうかを判定する *)
 (* equal_length : a' list -> a'list -> bool *)
 let equal_length = false
