0ee14fe37f3add8948b96ca1805d37523fce7407
ittyi <vioiv.k4@gmail.com>
2023-07-22 03:43:55 +0900

    📝 ex10.9: listの長さを計測する関数を定義開始

--- chap10/ex10_9.ml
+++ chap10/ex10_9.ml
@@ -1,3 +1,7 @@
+(* 目的: listの長さを計測する *)
+(* leng : a7 list -> int *)
+let rec leng lst = 0
+
 (* 目的: 二つのリストの長さが同じかどうかを判定する *)
 (* equal_length : a' list -> a'list -> bool *)
 let equal_length = false
