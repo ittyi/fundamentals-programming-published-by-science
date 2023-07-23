f731fe826717ccf77029eefeb8cfeb1923182dbf
ittyi <vioiv.k4@gmail.com>
2023-07-23 23:17:24 +0900

    📝 ex10.10: 目的と型定義の明記

---
+++ chap10/ex10_10.ml
@@ -0,0 +1,8 @@
+(* 
+ローマ字の駅名(文字列)と駅名リスト(ekimei_t list 型)を受け取ったら、
+その駅の漢字表記を文字列で返す関数
+*)
+(* romaji_to_kanji : string -> ekimei_t list -> string *)
+let romaji_to_kanji romaji_name lst = ""
+
+(* test: #use "ex10_10.ml";; *)
