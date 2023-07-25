6217fe68cfa111af559d274837741ae3bf7f7af7
ittyi <vioiv.k4@gmail.com>
2023-07-25 23:48:39 +0900

    📝 ex10.11: 目的と型定義の記載

---
+++ chap10/ex10_11.ml
@@ -0,0 +1,5 @@
+(* 目的: 漢字の駅名を二つと駅間リストを受け取ったら、その2駅の距離を返す関数 *)
+(* get_ekikan_kyori : string -> string -> ekimei_t list -> int *)
+let get_ekikan_kyori station1 station2 ekimei_list = 0
+
+(* test: #use "ex10_11.ml";; *)
