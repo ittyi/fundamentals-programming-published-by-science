324eeb6cd130a813b64b4937943e48f1e30c7d16
ittyi <vioiv.k4@gmail.com>
2023-07-28 03:29:49 +0900

    📝 ex10.11: 模範解答の追加

--- chap10/ex10_11.ml
+++ chap10/ex10_11.ml
@@ -206,3 +206,21 @@ let test1 = get_ekikan_kyori "" ""  global_ekikan_list = infinity
 let test2 = get_ekikan_kyori "営団赤塚" "営団成増"  global_ekikan_list = 1.5
 let test3 = get_ekikan_kyori "営団成増" "営団赤塚"  global_ekikan_list = 1.5
 let test4 = get_ekikan_kyori "平和台" "営団成増"  global_ekikan_list = infinity
+
+
+(* 以下模範回答 *)
+(* 目的：ふたつの駅の間の距離を求める *) 
+(* get_ekikan_kyori : string -> string -> ekikan_t list -> float *) 
+let rec get_ekikan_kyori_exsample eki1 eki2 lst = match lst with 
+    [] -> infinity 
+  | {kiten = k; shuten = s; keiyu = y; kyori = r; jikan = j} :: rest -> 
+     if (eki1 = k && eki2 = s) || (eki1 = s && eki2 = k) 
+     then r 
+     else get_ekikan_kyori eki1 eki2 rest 
+ 
+(* テスト *) 
+let test1_exsample = get_ekikan_kyori_exsample "茗荷谷" "新大塚" global_ekikan_list = 1.2 
+let test2_exsample = get_ekikan_kyori_exsample "茗荷谷" "池袋" global_ekikan_list = infinity 
+let test3_exsample = get_ekikan_kyori_exsample "東京" "大手町" global_ekikan_list = 0.6 
+
+(* あぁ、、確かにそれでできるわ、、 *)
