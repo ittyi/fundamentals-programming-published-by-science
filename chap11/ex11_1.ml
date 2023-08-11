00a2057c721169e00ac276e66c027460105e1778
ittyi <vioiv.k4@gmail.com>
2023-08-11 22:29:21 +0900

    ✅ ex11.1: add test

--- chap11/ex11_1.ml
+++ chap11/ex11_1.ml
@@ -2,3 +2,10 @@
 (* sum_of_square : int -> int *)
 let rec sum_of_square 0 = 0
 
+(* test: #use "ex11_1.ml";; *)
+let test1 = sum_of_square 0 = 0
+let test2 = sum_of_square 1 = 1
+let test3 = sum_of_square 2 = 5
+let test4 = sum_of_square 3 = 14
+let test5 = sum_of_square 4 = 30
+let test6 = sum_of_square 5 = 55
