dff623865b971adaa6dcbe99c22567c3976455be
ittyi <vioiv.k4@gmail.com>
2023-08-27 03:28:16 +0900

    ✅&🍱 ex12.2: テストデータとテストの追加

--- chap12/ex12_2.ml
+++ chap12/ex12_2.ml
@@ -1,3 +1,20 @@
+(* 一旦、上から四つだけとってくる *)
+type ekimei_t = {
+  kanji: string;
+  kana: string;
+  romaji: string;
+  shozoku: string;
+}
+
+let global_ekimei_list = [ 
+  {kanji="代々木上原"; kana="よよぎうえはら"; romaji="yoyogiuehara"; shozoku="千代田線"}; 
+  {kanji="代々木公園"; kana="よよぎこうえん"; romaji="yoyogikouen"; shozoku="千代田線"}; 
+  {kanji="明治神宮前"; kana="めいじじんぐうまえ"; romaji="meijijinguumae"; shozoku="千代田線"}; 
+  {kanji="表参道"; kana="おもてさんどう"; romaji="omotesandou"; shozoku="千代田線"}; 
+  {kanji="乃木坂"; kana="のぎざか"; romaji="nogizaka"; shozoku="千代田線"}; 
+]
+
+
 (*  現在の最短距離や最短経路を保持するレコード *)
 (* 頂点の集合はこのデータのリストになる *)
 type eki_t = {
@@ -9,4 +26,19 @@ type eki_t = {
 (* 目的: ekimei_t list を受け取り、 eki_t の list を作成する関数 *)
 (* 仕様: saitan_kyori は無限大、 temae_list は空のリスト *)
 (* make_eki_list : ekimei_t list -> eki_t list *)
-let make_eki_list lst = lst
+let make_eki_list lst = []
+
+(* test: #use "ex12_2.ml";; *)
+let test1 = make_eki_list [
+  {kanji="代々木上原"; kana="よよぎうえはら"; romaji="yoyogiuehara"; shozoku="千代田線"}; 
+  {kanji="代々木公園"; kana="よよぎこうえん"; romaji="yoyogikouen"; shozoku="千代田線"}; 
+  {kanji="明治神宮前"; kana="めいじじんぐうまえ"; romaji="meijijinguumae"; shozoku="千代田線"}; 
+  {kanji="表参道"; kana="おもてさんどう"; romaji="omotesandou"; shozoku="千代田線"}; 
+  {kanji="乃木坂"; kana="のぎざか"; romaji="nogizaka"; shozoku="千代田線"}; 
+] = [
+  {namae="代々木上原"; saitan_kyori=infinity; temae_list=[""]};
+  {namae="代々木公園"; saitan_kyori=infinity; temae_list=[""]};
+  {namae="明治神宮前"; saitan_kyori=infinity; temae_list=[""]};
+  {namae="表参道"; saitan_kyori=infinity; temae_list=[""]};
+  {namae="乃木坂"; saitan_kyori=infinity; temae_list=[""]}; 
+]
