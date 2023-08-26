f3112dda6073851606ab8e59952e54c89539fb19
ittyi <vioiv.k4@gmail.com>
2023-08-27 03:30:34 +0900

    🚧 ex12.2: 一旦、駅名の配列を作成

--- chap12/ex12_2.ml
+++ chap12/ex12_2.ml
@@ -26,7 +26,9 @@ type eki_t = {
 (* 目的: ekimei_t list を受け取り、 eki_t の list を作成する関数 *)
 (* 仕様: saitan_kyori は無限大、 temae_list は空のリスト *)
 (* make_eki_list : ekimei_t list -> eki_t list *)
-let make_eki_list lst = []
+let rec make_eki_list lst = match lst with
+| [] -> []
+| first :: rest -> first.kanji :: make_eki_list rest
 
 (* test: #use "ex12_2.ml";; *)
 let test1 = make_eki_list [
@@ -35,10 +37,12 @@ let test1 = make_eki_list [
   {kanji="明治神宮前"; kana="めいじじんぐうまえ"; romaji="meijijinguumae"; shozoku="千代田線"}; 
   {kanji="表参道"; kana="おもてさんどう"; romaji="omotesandou"; shozoku="千代田線"}; 
   {kanji="乃木坂"; kana="のぎざか"; romaji="nogizaka"; shozoku="千代田線"}; 
-] = [
+] 
+
+(* = [
   {namae="代々木上原"; saitan_kyori=infinity; temae_list=[""]};
   {namae="代々木公園"; saitan_kyori=infinity; temae_list=[""]};
   {namae="明治神宮前"; saitan_kyori=infinity; temae_list=[""]};
   {namae="表参道"; saitan_kyori=infinity; temae_list=[""]};
   {namae="乃木坂"; saitan_kyori=infinity; temae_list=[""]}; 
-]
+] *)
