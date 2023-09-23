38652a2f3ef026662e4f8dd773a90a6ba0547af2
ittyi <vioiv.k4@gmail.com>
2023-09-24 06:24:33 +0900

    ✨ ex13.1: 関数count_ketsueki 完成

--- chap13/ex13_1.ml
+++ chap13/ex13_1.ml
@@ -7,21 +7,21 @@ type parson_t = {
 }
 
 let test = [{
-    name = "string";
+    name = "string1";
     height = 170;
     body_weight = 56;
     birthday = "11/15";
     blood_type = "O";
-  },
+  };
   {
-    name = "string";
+    name = "string2";
     height = 170;
     body_weight = 56;
     birthday = "11/15";
     blood_type = "O";
-  },
+  };
   {
-    name = "string";
+    name = "string3";
     height = 170;
     body_weight = 56;
     birthday = "11/15";
@@ -31,7 +31,11 @@ let test = [{
 
 (* 目的: parson_t 型の list を受け取り、指定した血液型の数を返す *)
 (* count_ketsueki : parson_t list -> int *)
-let count_ketsueki lst blood_type = 0
+let rec count_ketsueki lst blood_type = match lst with
+| [] -> 0
+| first :: rest -> if first.blood_type = blood_type
+    then 1 + (count_ketsueki rest blood_type)
+    else count_ketsueki rest blood_type
 
 (* test: #use "ex13_1.ml";; *)
 let test1 = count_ketsueki test "A" = 1
