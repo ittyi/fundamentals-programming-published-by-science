8fd43890ab1029cef5dbf7e419cbf28a5df19c7a
ittyi <vioiv.k4@gmail.com>
2023-09-24 06:13:31 +0900

    🍱 ex13.1: 8.3から必要な方とテストデータを引っ張る

---
+++ chap13/ex13_1.ml
@@ -0,0 +1,32 @@
+type parson_t = {
+  name: string;
+  height: int;
+  body_weight: int;
+  birthday: string;
+  blood_type: string;
+}
+
+let test = [{
+    name = "string";
+    height = 170;
+    body_weight = 56;
+    birthday = "11/15";
+    blood_type = "O";
+  },
+  {
+    name = "string";
+    height = 170;
+    body_weight = 56;
+    birthday = "11/15";
+    blood_type = "O";
+  },
+  {
+    name = "string";
+    height = 170;
+    body_weight = 56;
+    birthday = "11/15";
+    blood_type = "A";
+  }
+]
+
+(* test: #use "ex13_1.ml";; *)
