356eb567ff935fdd5ef9668609de54c705955420
ittyi <vioiv.k4@gmail.com>
2023-09-24 06:17:43 +0900

    ✅ ex13.1: add test data

--- chap13/ex13_1.ml
+++ chap13/ex13_1.ml
@@ -31,6 +31,9 @@ let test = [{
 
 (* 目的: parson_t 型の list を受け取り、指定した血液型の数を返す *)
 (* count_ketsueki : parson_t list -> int *)
-let count_ketsueki lst = 0
+let count_ketsueki lst blood_type = 0
 
 (* test: #use "ex13_1.ml";; *)
+let test1 = count_ketsueki test "A" = 1
+let test2 = count_ketsueki test "B" = 0
+let test3 = count_ketsueki test "O" = 2
