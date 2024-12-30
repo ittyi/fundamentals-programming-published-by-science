2d354143f433926f7668cf61dde272db51fb48a4
ittyi <vioiv.k4@gmail.com>
2024-12-30 23:38:37 +0900

    🎨 feat: [ex2_1.ml] 答えを出力するように変更
    途中で出たエラーにも対応し、string_of_int を利用
    This expression has type int but an expression was expected of type stringocamllsp
    int

--- chap2/ex2_1.ml
+++ chap2/ex2_1.ml
@@ -1,3 +1,8 @@
-7 - 3 * 4;;
-7 / 2 * 2;;
-7 * 2 / 2;;
+let x = 7 - 3 * 4;;
+let y = 7 / 2 * 2;;
+let z = 7 * 2 / 2;;
+
+print_endline "Hello, World!";;
+print_endline (string_of_int x);;
+print_endline (string_of_int y);;
+print_endline (string_of_int z);;
