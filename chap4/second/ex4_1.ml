951456ae080a1dfc8e234db1c823737574b616be
ittyi <vioiv.k4@gmail.com>
2024-12-30 23:59:52 +0900

    ✨ [ex4_1.ml] 肩慣らし。できた

--- chap4/second/ex4_1.ml
+++ chap4/second/ex4_1.ml
@@ -1,6 +1,8 @@
+let get_hourly_wage year = 850 + (year * 100)
+
 (* val baito_kyuyo : 'a -> 'a = <fun> *)
-let baito_kyuyo year time_h = 1 ;;
+let baito_kyuyo year time_h = (get_hourly_wage year) * time_h ;;
 
 (* test: ocaml chap4/second/ex4_1.ml *)
-let test1 = baito_kyuyo 1 1 = 1;;
-print_endline (string_of_bool test1);;
+let test1 = baito_kyuyo 1 1;;
+print_endline (string_of_int test1);;
