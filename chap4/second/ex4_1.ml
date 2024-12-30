eaf597b3eb6888e12c0cf185d77c59985f64127d
ittyi <vioiv.k4@gmail.com>
2024-12-31 00:01:57 +0900

    ✅ add: [ex4_1.ml] 肩慣らし テストの作成

--- chap4/second/ex4_1.ml
+++ chap4/second/ex4_1.ml
@@ -4,5 +4,11 @@ let get_hourly_wage year = 850 + (year * 100)
 let baito_kyuyo year time_h = (get_hourly_wage year) * time_h ;;
 
 (* test: ocaml chap4/second/ex4_1.ml *)
-let test1 = baito_kyuyo 1 1;;
-print_endline (string_of_int test1);;
+let test1 = baito_kyuyo 1 1 = 950;;
+print_endline (string_of_bool test1);;
+
+let test2 = baito_kyuyo 1 2 = 1900;;
+print_endline (string_of_bool test2);;
+
+let test2 = baito_kyuyo 2 2 = 2100;;
+print_endline (string_of_bool test2);;
