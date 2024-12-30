46cc27e153562862c96bd04898092fd07c5d9b5b
ittyi <vioiv.k4@gmail.com>
2024-12-30 23:57:26 +0900

    🚧 add:  [ex4_1.ml] 肩慣らし。引数を正しく年数と時間に変更してグリーンテストを通した

--- chap4/second/ex4_1.ml
+++ chap4/second/ex4_1.ml
@@ -1,6 +1,6 @@
 (* val baito_kyuyo : 'a -> 'a = <fun> *)
-let baito_kyuyo x = x ;;
+let baito_kyuyo year time_h = 1 ;;
 
 (* test: ocaml chap4/second/ex4_1.ml *)
-let test1 = baito_kyuyo 1 = 1;;
+let test1 = baito_kyuyo 1 1 = 1;;
 print_endline (string_of_bool test1);;
