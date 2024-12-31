756639bfa371e1bbce332f7ffee90a1e03e71d1a
ittyi <vioiv.k4@gmail.com>
2024-12-31 23:49:52 +0900

    ♻️ refactor: [ex4_3.ml] 関数名をちょっと変更

--- chap4/second/ex4_3.ml
+++ chap4/second/ex4_3.ml
@@ -1,6 +1,6 @@
 print_endline "Hello, World!";;
 
-let hyojun_taiju m = m *. m *. 22.0;;
+let hyojun_taiju_for_float m = m *. m *. 22.0;;
 
 (* let test1 = hyojun_taiju 1 = 22;;
 print_endline (string_of_bool test1);;
@@ -8,5 +8,5 @@ print_endline (string_of_bool test1);;
 let test2 = hyojun_taiju 170 = 635800;;
 print_endline (string_of_bool test2);; *)
 
-let test3 = string_of_float (hyojun_taiju 1.7) = "63.58";;
+let test3 = string_of_float (hyojun_taiju_for_float 1.7) = "63.58";;
 print_endline (string_of_bool test3);;
