fef02596c4a0e22c8c580a592ebf4d6d64a921af
ittyi <vioiv.k4@gmail.com>
2024-12-31 23:33:40 +0900

    ♻️ ✅ refactor: [ex4_2.ml] 変数名とテストケースを追加

--- chap4/second/ex4_2.ml
+++ chap4/second/ex4_2.ml
@@ -1,6 +1,6 @@
 print_endline "Hello, World!";;
 
-let jikoshokai s = "Hello, " ^ s;;
+let jikoshokai name = "Hello, " ^ name;;
 
 let test1 = jikoshokai "test" = "Hello, test";;
 print_endline (string_of_bool test1);;
@@ -10,3 +10,6 @@ print_endline (string_of_bool test2);;
 
 let test3 = jikoshokai "ttest" = "Hello, ttest";;
 print_endline (string_of_bool test3);;
+
+let test4 = jikoshokai "" = "Hello, ";;
+print_endline (string_of_bool test4);;
