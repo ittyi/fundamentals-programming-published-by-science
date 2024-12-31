941d9d314c74c94a0008d614042f66de1c9e29c8
ittyi <vioiv.k4@gmail.com>
2024-12-31 23:30:53 +0900

    ✅ add: [ex4_2.ml] red test

--- chap4/second/ex4_2.ml
+++ chap4/second/ex4_2.ml
@@ -2,5 +2,11 @@ print_endline "Hello, World!";;
 
 let jikoshokai s = s;;
 
-let test1 = jikoshokai "test" = "test";;
+let test1 = jikoshokai "test" = "Hello, test";;
 print_endline (string_of_bool test1);;
+
+let test2 = jikoshokai "test2" = "Hello, test2";;
+print_endline (string_of_bool test2);;
+
+let test3 = jikoshokai "ttest" = "Hello, ttest";;
+print_endline (string_of_bool test3);;
