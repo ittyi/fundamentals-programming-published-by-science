7530566876e15e238ad73c2d6226a462ffa224a9
ittyi <vioiv.k4@gmail.com>
2024-12-31 23:53:13 +0900

    出力結果にて、少し説明を追加

--- chap4/second/ex4_3.ml
+++ chap4/second/ex4_3.ml
@@ -1,14 +1,14 @@
-print_endline "Hello, World!";;
-
 let hyojun_taiju_for_float weight = weight *. weight *. 22.0;;
 
 let hyojun_taiju_for_int weight = weight * weight * 22;;
 
+print_endline "test hyojun_taiju_for_int";;
 let test1 = hyojun_taiju_for_int 1 = 22;;
 print_endline (string_of_bool test1);;
 
 let test2 = hyojun_taiju_for_int 170 = 635800;;
 print_endline (string_of_bool test2);;
 
+print_endline "test hyojun_taiju_for_float";;
 let test3 = string_of_float (hyojun_taiju_for_float 1.7) = "63.58";;
 print_endline (string_of_bool test3);;
