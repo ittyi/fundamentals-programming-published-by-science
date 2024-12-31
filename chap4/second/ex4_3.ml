102f572f36f3631b84d05cf0e23f2cf7361bd01d
ittyi <vioiv.k4@gmail.com>
2024-12-31 23:46:44 +0900

    え？なんでこれ false なんだろう。。

--- chap4/second/ex4_3.ml
+++ chap4/second/ex4_3.ml
@@ -1,6 +1,6 @@
 print_endline "Hello, World!";;
 
-let hyojun_taiju m = m *. m;;
+let hyojun_taiju m = m *. m *. 22.0;;
 
 (* let test1 = hyojun_taiju 1 = 22;;
 print_endline (string_of_bool test1);;
@@ -8,5 +8,6 @@ print_endline (string_of_bool test1);;
 let test2 = hyojun_taiju 170 = 635800;;
 print_endline (string_of_bool test2);; *)
 
-(* let test3 = hyojun_taiju 1.7 = 0.1;;
-print_endline (string_of_bool test3);; *)
+let test3 = hyojun_taiju 1.7 = 63.58;;
+print_endline (string_of_float (hyojun_taiju 1.7));;
+print_endline (string_of_bool test3);;
