e692b53f61509f54536658334459fd5143edc4e5
ittyi <vioiv.k4@gmail.com>
2024-12-31 23:43:41 +0900

    code save

--- chap4/second/ex4_3.ml
+++ chap4/second/ex4_3.ml
@@ -1,9 +1,12 @@
 print_endline "Hello, World!";;
 
-let hyojun_taiju m = m * m * 22;;
+let hyojun_taiju m = m * m;;
 
-let test1 = hyojun_taiju 1 = 22;;
+(* let test1 = hyojun_taiju 1 = 22;;
 print_endline (string_of_bool test1);;
 
 let test2 = hyojun_taiju 170 = 635800;;
-print_endline (string_of_bool test2);;
+print_endline (string_of_bool test2);; *)
+
+(* let test3 = hyojun_taiju 1.7 = 0.1;;
+print_endline (string_of_bool test3);; *)
