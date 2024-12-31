5dd021198440b02d50905f487d7e47cfbd8ab9ac
ittyi <vioiv.k4@gmail.com>
2024-12-31 23:40:07 +0900

    ✅ add: [ex4_3.ml] テストケース追加

--- chap4/second/ex4_3.ml
+++ chap4/second/ex4_3.ml
@@ -4,3 +4,6 @@ let hyojun_taiju m = m * m * 22;;
 
 let test1 = hyojun_taiju 1 = 22;;
 print_endline (string_of_bool test1);;
+
+let test2 = hyojun_taiju 170 = 635800;;
+print_endline (string_of_bool test2);;
