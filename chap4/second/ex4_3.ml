b6e18fdb4a12cfc142b7ad300f0f796c4a9569e3
ittyi <vioiv.k4@gmail.com>
2024-12-31 23:50:40 +0900

    fix: [ex4_3.ml] int も用意しておいた

--- chap4/second/ex4_3.ml
+++ chap4/second/ex4_3.ml
@@ -2,11 +2,13 @@ print_endline "Hello, World!";;
 
 let hyojun_taiju_for_float m = m *. m *. 22.0;;
 
-(* let test1 = hyojun_taiju 1 = 22;;
+let hyojun_taiju_for_int m = m * m * 22;;
+
+let test1 = hyojun_taiju_for_int 1 = 22;;
 print_endline (string_of_bool test1);;
 
-let test2 = hyojun_taiju 170 = 635800;;
-print_endline (string_of_bool test2);; *)
+let test2 = hyojun_taiju_for_int 170 = 635800;;
+print_endline (string_of_bool test2);;
 
 let test3 = string_of_float (hyojun_taiju_for_float 1.7) = "63.58";;
 print_endline (string_of_bool test3);;
