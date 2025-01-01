03a9e7c759eeb642317c3c4c577b49e8ba51d8f4
ittyi <vioiv.k4@gmail.com>
2025-01-01 23:43:51 +0900

    ⚗️ add: [ex7_1.ml] タプルの値取得方法で、標準関数に fst, snd なるものがあった。3番目以降を取得する標準関数はないらしい。

--- chap7/second/ex7_1.ml
+++ chap7/second/ex7_1.ml
@@ -1,4 +1,4 @@
-print_endline "ex7-1 goukei_to_heikin";;
+let () = print_endline "ex7-1 goukei_to_heikin";;
 
 (* 5教科を与えると、平均と合計を組にして返す *)
 let goukei_to_heikin japanese mathematics science social_studies english = 
@@ -10,3 +10,8 @@ let goukei_to_heikin japanese mathematics science social_studies english =
 
 let test1 = goukei_to_heikin 50 50 50 50 50 = (50, 250);;
 print_endline (string_of_bool test1);;
+
+
+print_endline "標準関数 fst, snd を使った方法。3 番目以降を取得する標準関数はないらしい。";;
+let test2 = goukei_to_heikin 50 50 50 50 50;;
+let () = Printf.printf "平均: %d, 合計: %d\n" (fst test2) (snd test2)
