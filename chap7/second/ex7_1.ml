0a775866a6759b75181ece8a707f1485f86b8ed1
ittyi <vioiv.k4@gmail.com>
2025-01-01 23:24:13 +0900

    ✨ feat: [ex7_1.ml] まずは愚直に実装

--- chap7/second/ex7_1.ml
+++ chap7/second/ex7_1.ml
@@ -1,7 +1,11 @@
-print_endline "Hello, World!";;
+print_endline "ex7-1 goukei_to_heikin";;
 
 (* 5教科を与えると、平均と合計を組にして返す *)
-let goukei_to_heikin japanese mathematics science social_studies english = (50, 250);;
+let goukei_to_heikin japanese mathematics science social_studies english = 
+    (
+      (japanese + mathematics + science + social_studies + english) / 5, 
+      (japanese + mathematics + science + social_studies + english)
+    );;
 
 let test1 = goukei_to_heikin 50 50 50 50 50 = (50, 250);;
 print_endline (string_of_bool test1);;
