399af00a6f6fbcab70377f7307d38b085a139e5e
ittyi <vioiv.k4@gmail.com>
2025-01-01 23:27:44 +0900

    ♻️ refactor:  [ex7_1.ml] 局所変数定義を使ってよりスマートに

--- chap7/second/ex7_1.ml
+++ chap7/second/ex7_1.ml
@@ -2,9 +2,10 @@ print_endline "ex7-1 goukei_to_heikin";;
 
 (* 5教科を与えると、平均と合計を組にして返す *)
 let goukei_to_heikin japanese mathematics science social_studies english = 
+  let goukei = (japanese + mathematics + science + social_studies + english) in 
     (
-      (japanese + mathematics + science + social_studies + english) / 5, 
-      (japanese + mathematics + science + social_studies + english)
+      goukei / 5, 
+      goukei
     );;
 
 let test1 = goukei_to_heikin 50 50 50 50 50 = (50, 250);;
