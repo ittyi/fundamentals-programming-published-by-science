4768669ca67548662fbfccddfada34709c3b7b18
ittyi <vioiv.k4@gmail.com>
2025-01-01 23:44:20 +0900

    ⚗️ add: [ex7_1.ml] タプルの値取得方法でおそらくいちばんオーソドックスなパターンマッチを仕様

--- chap7/second/ex7_1.ml
+++ chap7/second/ex7_1.ml
@@ -12,6 +12,11 @@ let test1 = goukei_to_heikin 50 50 50 50 50 = (50, 250);;
 print_endline (string_of_bool test1);;
 
 
-print_endline "標準関数 fst, snd を使った方法。3 番目以降を取得する標準関数はないらしい。";;
+print_endline "標準関数 fst, snd を使った方法。3番目以降を取得する標準関数はないらしい。";;
 let test2 = goukei_to_heikin 50 50 50 50 50;;
-let () = Printf.printf "平均: %d, 合計: %d\n" (fst test2) (snd test2)
+let () = Printf.printf "平均: %d, 合計: %d\n" (fst test2) (snd test2);;
+
+print_endline "パターンマッチを使った方法。たぶん一番普通";;
+let  test3 = goukei_to_heikin 50 50 50 50 50;;
+let (average, total)= test3;;
+let () = Printf.printf "平均: %d, 合計: %d\n" average total;;
