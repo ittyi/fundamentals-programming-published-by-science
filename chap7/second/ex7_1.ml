45b2c49f9ec3036987277411770b697acc102f53
ittyi <vioiv.k4@gmail.com>
2025-01-01 23:47:57 +0900

    ✅ add:  [ex7_1.ml] 一応テストも書いておく

--- chap7/second/ex7_1.ml
+++ chap7/second/ex7_1.ml
@@ -20,3 +20,15 @@ print_endline "パターンマッチを使った方法。たぶん一番普通";
 let  test3 = goukei_to_heikin 50 50 50 50 50;;
 let (average, total)= test3;;
 let () = Printf.printf "平均: %d, 合計: %d\n" average total;;
+print_endline (string_of_bool (test3 = (50, 250)));;
+
+let  test4 = goukei_to_heikin 50 60 70 80 90;;
+let (average, total)= test4;;
+let () = Printf.printf "平均: %d, 合計: %d\n" average total;;
+print_endline (string_of_bool (test4 = (70,350)));;
+
+
+let  test5 = goukei_to_heikin 100 60 70 80 90;;
+let (average, total)= test5;;
+let () = Printf.printf "平均: %d, 合計: %d\n" average total;;
+print_endline (string_of_bool (test5 = (80,400)));;
