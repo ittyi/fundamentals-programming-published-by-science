f526c009aa08fbab1e46b9a2f117c40024cfb2a1
ittyi <vioiv.k4@gmail.com>
2025-01-02 23:14:25 +0900

    🎨 add: [ex8_6] テンプレート追加

--- metronetwork/ekimei.ml
+++ metronetwork/ekimei.ml
@@ -4,12 +4,17 @@ type ekimei_t = {
   romaji: string;
   shozoku: string;
 } ;;
-(* {kanji: "茗荷谷"; kana: "test"; romaji: "test"; shozoku: "test";} *)
+(* {kanji="茗荷谷"; kana="みょうがだに"; romaji="myogadani"; shozoku="丸の内線"} *)
 
 (* 目的：ekimei_tを受け取ったら「路線名, 駅名（かな）」で返す *)
-let hyoji ekimei = "路線名, 駅名（かな）" ;;
+let hyoji ekimei = match ekimei with
+| {kanji=kan; kana=ka; romaji=r; shozoku=s;} -> "" ;;
 
 print_endline "func hyouji";;
-let  test1 = hyoji "";;
+let test1 = hyoji {kanji="駅名"; kana="かな"; romaji="test"; shozoku="路線名"};;
 let () = Printf.printf "駅: %s\n" test1;;
 print_endline (string_of_bool (test1 = "路線名, 駅名（かな）"));;
+
+let  test2 = hyoji {kanji="茗荷谷"; kana="みょうがだに"; romaji="myogadani"; shozoku="丸の内線"};;
+let () = Printf.printf "駅: %s\n" test2;;
+print_endline (string_of_bool (test2 = "丸の内線, 茗荷谷（みょうがだに）"));;
