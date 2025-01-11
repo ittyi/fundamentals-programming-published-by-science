61a0b22879e7e135b0d12e3dc5b20e9efe40f620
ittyi <vioiv.k4@gmail.com>
2025-01-11 23:26:25 +0900

    🚧 fix: [なんかやってみたくなった] uとvを間違えていたのでシャッフル。さらに、最短距離がまだ確定していない点の集合は違うデータ構造だったので、一旦隔離してメトロネックワークを作成

--- metronetwork/original.ml
+++ metronetwork/original.ml
@@ -6,19 +6,22 @@ type confirmed_t = {
   value : int;
 } ;;
 
-let v =[
+let u =[
   {name = "a"; value=0};
 ];;  
 
 
 (* 最短距離がまだ確定していない点の集合 *)
-type unconfirmed_t = {
+
+
+(* 最短距離がまだ確定していない点の集合 *)
+type metro_network_t = {
   start  : string;
   destination : string;
   value : int;
 } ;;
 
-let u =[
+let metro_network =[
   {start = "a"; destination="b"; value=10};
   {start = "a"; destination="d"; value=4};
   {start = "b"; destination="c"; value=2};
@@ -33,3 +36,24 @@ let shortest_distance start destination = "";;
 let test1 = shortest_distance "" "";;
 let () = Printf.printf "test1: %s\n" test1;;
 print_endline (string_of_bool (test1 = ""));;
+
+(* さてさて。
+ここからどうやっていくか考える。
+あーまずvが違うか。
+Uは、a->b, a-> c, a->d などの全てのパターンであるべきで、おそらく今のvは入力になるべき。
+
+なので、入力はstart destination metro_network って感じかな。
+
+なのでまず、 metro_network を受け取ったらv（最短距離がまだ確定していない点の集合）を作る関数が必要。
+・この関数により、全ての点と点のパターンがわかる。
+・この関数時点では、点と点の距離まではまだわからない。
+
+
+次に、開始地点（start）....
+
+
+ポイントは、
+
+
+次に、
+*)
