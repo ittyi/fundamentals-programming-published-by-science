74d9afd087f1cade240228ea1b7b5f0f9cefd09c
ittyi <vioiv.k4@gmail.com>
2025-01-20 01:11:25 +0900

    code clean

--- metronetwork/original.ml
+++ metronetwork/original.ml
@@ -22,7 +22,7 @@ let string_of_shortest_distance record =
     (string_of_list record.routes)
     record.value
 
-(* shortest_distance_t のリストを文字列化する関数 *)
+(* debug Utility 関数: shortest_distance_t のリストを文字列化する関数 *)
 let string_of_shortest_distance_list records =
   let rec aux lst =
     match lst with
@@ -32,11 +32,11 @@ let string_of_shortest_distance_list records =
   in
   "[" ^ aux records ^ "]"
 
-(* shortest_distance_t をデバッグ用に出力する関数 *)
+(* debug Utility 関数: shortest_distance_t をデバッグ用に出力する関数 *)
 let print_shortest_distance record =
   print_endline (string_of_shortest_distance record)
 
-(* shortest_distance_t のリストをデバッグ用に出力する関数 *)
+(* debug Utility 関数: shortest_distance_t のリストをデバッグ用に出力する関数 *)
 let print_shortest_distance_list records =
   print_endline (string_of_shortest_distance_list records)
 
@@ -91,6 +91,8 @@ print_endline (string_of_bool (init_u_test2 = u));;
 let init_u_test3 = init_u "b" metro_network;;
 let () = Printf.printf "init_u_test3: ";;
 print_endline (string_of_bool (init_u_test3 = [{name = "b"; routes=[]; value=0}]));;
+
+
 
 (* 文字列配列の中に既に入っているかを確認する関数 *)
 let rec contain list chech_str = match list with
@@ -151,12 +153,12 @@ let get_metro_network_pattern_test2 = get_metro_network_pattern metro_network;;
 let () = Printf.printf "get_metro_network_pattern_test2: ";;
 print_endline (string_of_bool (get_metro_network_pattern_test2 = ["d"; "e"; "c"; "b"; "a"]));;
 
-(* 実行と標準出力 *)
 let () =
   let result = get_metro_network_pattern metro_network in
   Printf.printf "get_metro_network_pattern result: [%s]\n" (string_of_list result)
 
-(* metro_network の重複なし全パターンを受け取ったら、 shortest_distance_t list の形に初期化する関数 *)
+(* 開始地点を受け取って、最短距離がまだ確定していない点の集合 v を初期化する関数 *)
+(* metro_network の重複なし全パターンを受け取ったら、 shortest_distance_t list の形に初期化する *)
 let rec init_v start metro_network_pattern = match metro_network_pattern with
 | [] -> []
 | f :: r -> if f = start
@@ -167,64 +169,6 @@ let init_v_test1 = let result = get_metro_network_pattern metro_network in init_
 let () = Printf.printf "init_v_test1: ";;
 print_endline (string_of_bool (init_v_test1 = v) );;
 
-(* 開始地点を受け取って、最短距離がまだ確定していない点の集合 v を初期化する関数 *)
-(* let set_init_v start metro_network = match metro_network with
-| [] -> []
-| f :: r -> if start = ;;
-
-let set_init_v_test1 = set_init_v "" metro_network;;
-let () = Printf.printf "set_init_v_test1: ";;
-print_endline (string_of_bool (set_init_v_test1 = v));; *)
-
-(* さてさて。
-ここからどうやっていくか考える。
-
-あーまずvが違うか。
-Uは、a->b, a-> c, a->d などの全てのパターンであるべきで、おそらく今のvは入力になるべき。
-
-なので、入力はstart destination metro_network って感じかな。
-
-なのでまず、 metro_network を受け取ったらv（最短距離がまだ確定していない点の集合）を作る関数が必要。
-・この関数により、全ての点と点のパターンがわかる。
-・この関数時点では、点と点の距離まではまだわからない。
-
-次に、開始地点（start）....
-
-
-最短距離がまだ確定していない点の集合 v が持つべきデータは、
-入力として受け取った start から destination までの全部のパターンなのでは？
-
-
-仮置きで、A -> Cまでの最短経路を求めることにする。
-A -> B -> C だと12なのに対して、 A -> D -> E -> C の方が8なので短い。
-
-Step 1.
-Uに点A
-Vに他の点が入る。
-
-この時、Uを作成するロジックとVを作成するロジックがまず必要。
-まだOCamlでの連結リストの作り方がわからないので、とりあえずレコードのリストにする。
-
-それぞれに必要なデータを整理する。
-【U】
-・ start から全ての点までの最短経路をもてれば良い。
-・つまり、start が A ならA以外の点と、一旦未定の状態で空配列（通る道のリスト）と最短距離（通る道を全て足した数字）が必要
-
-【V】
-・最初は start の点までの最短経路をもてれば良い。
-・つまり、start が A なら A と確定の状態で空配列と最短距離＝0が必要
-
-Step 2.
-
-Step 3.
-
-Step 4.
-
-Step 5.
-
-
-次に、
-*)
 
 
 (* 開始地点と終了地点を受け取って、一旦 u, v を作成するところまでやる関数 *)
@@ -241,3 +185,5 @@ let () =
 
   print_endline "\nList of records:";
   shortest_distance "a" "b";
+
+(* u, v はできたので次 *)
