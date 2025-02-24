4a25cd316a82271f0f01cd4c93a4141ce02270e3
ittyi <vioiv.k4@gmail.com>
2025-02-25 00:00:28 +0900

    small fix

--- chap13/second/chap13_1.ml
+++ chap13/second/chap13_1.ml
@@ -7,7 +7,7 @@ type gakusei_t = {
 
 (* テストデータ *)
 let lst1 = []
-let lst2 = [{namae = "asai"; tensuu = 70; seiseki = "A"}; {namae = "asai"; tensuu = 70; seiseki = "A"}]
+let lst2 = [{namae = "asai"; tensuu = 70; seiseki = "A"}; {namae = "asai"; tensuu = 70; seiseki = "A"}; {namae = "asai"; tensuu = 70; seiseki = "B"}]
 
 (* 目的: 学生リスト lst のうち、与えられた入力と成績が一致する人の数を返す *)
 (* count_evaluation : gakusei_t list -> string -> int *)
@@ -25,6 +25,10 @@ print_endline (string_of_bool count_evaluation_test1);;
 let count_evaluation_test2 = count_evaluation lst2 "A" = 2;;
 let () = Printf.printf "count_evaluation_test2: ";;
 print_endline (string_of_bool count_evaluation_test2);;
+
+let count_evaluation_test3 = count_evaluation lst2 "B" = 1;;
+let () = Printf.printf "count_evaluation_test3: ";;
+print_endline (string_of_bool count_evaluation_test3);;
 
 (* 目的: 学生リスト lst のうち成績が A の人の数を返す *)
 (* count_A : gakusei_t list -> int *)
