b0873b472692bd7870a28e0e8894b21d7a47f0cb
ittyi <vioiv.k4@gmail.com>
2025-02-24 23:59:45 +0900

    🎨 fix: 既存の一般化した関数は、こういう形にすれば既存で使用している箇所全てを変えなくても修正が可能。確かにすぎて結構びっくりした

--- chap13/second/chap13_1.ml
+++ chap13/second/chap13_1.ml
@@ -5,29 +5,9 @@ type gakusei_t = {
   seiseki : string;     (* 成績 *) 
 } 
 
-(* 目的: 学生リスト lst のうち成績が A の人の数を返す *)
-(* count_A : gakusei_t list -> int *)
-let rec count_A lst = match lst with
-| [] -> 0
-| {namae = n; tensuu = t; seiseki = s} :: rest ->
-    if s = "A" then 1 + count_A rest
-               else count_A rest
-
 (* テストデータ *)
 let lst1 = []
 let lst2 = [{namae = "asai"; tensuu = 70; seiseki = "A"}; {namae = "asai"; tensuu = 70; seiseki = "A"}]
-
-(* テスト *)
-let test1 = count_A lst1 = 0
-let test2 = count_A lst2 = 1
-
-let test1 = count_A lst1 = 0;;
-let () = Printf.printf "test1: ";;
-print_endline (string_of_bool test1);;
-
-let test2 = count_A lst2 = 2;;
-let () = Printf.printf "test2: ";;
-print_endline (string_of_bool test2);;
 
 (* 目的: 学生リスト lst のうち、与えられた入力と成績が一致する人の数を返す *)
 (* count_evaluation : gakusei_t list -> string -> int *)
@@ -45,3 +25,24 @@ print_endline (string_of_bool count_evaluation_test1);;
 let count_evaluation_test2 = count_evaluation lst2 "A" = 2;;
 let () = Printf.printf "count_evaluation_test2: ";;
 print_endline (string_of_bool count_evaluation_test2);;
+
+(* 目的: 学生リスト lst のうち成績が A の人の数を返す *)
+(* count_A : gakusei_t list -> int *)
+(* let rec count_A lst = match lst with
+| [] -> 0
+| {namae = n; tensuu = t; seiseki = s} :: rest ->
+    if s = "A" then 1 + count_A rest
+               else count_A rest *)
+let rec count_A lst = count_evaluation lst "A"
+
+(* テスト *)
+let test1 = count_A lst1 = 0
+let test2 = count_A lst2 = 1
+
+let test1 = count_A lst1 = 0;;
+let () = Printf.printf "test1: ";;
+print_endline (string_of_bool test1);;
+
+let test2 = count_A lst2 = 2;;
+let () = Printf.printf "test2: ";;
+print_endline (string_of_bool test2);;
