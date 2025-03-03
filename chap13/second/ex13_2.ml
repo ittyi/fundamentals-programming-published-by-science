39a387c55bea2c4c68eea3ee5ccca91ff4a27402
ittyi <vioiv.k4@gmail.com>
2025-03-03 23:22:53 +0900

    ✅ test: [ex13_2] person_namae の red test を追加

--- chap13/second/ex13_2.ml
+++ chap13/second/ex13_2.ml
@@ -22,18 +22,28 @@ let test2 = {
   blood_type = "o";
 }
 
-let list = [test; test2]
+let test3 = {
+  name = "int";
+  height = 170;
+  body_weight = 56;
+  birthday = "11/15";
+  blood_type = "o";
+}
+
+let list1 = [test; test2]
+
+let list2 = [test; test2; test3]
 
 let rec person_namae lst = match lst with
-| [] -> 0
+| [] -> []
 | f :: r -> if f.height = 0
-    then 0
-    else 0
+    then []
+    else []
 
-let test1 = person_namae list = 0;;
+let test1 = person_namae list1 = ["string"];;
 let () = Printf.printf "test1: ";;
 print_endline (string_of_bool test1);;
 
-let test2 = person_namae list = 2;;
+let test2 = person_namae list2 = ["string"; "int"];;
 let () = Printf.printf "test2: ";;
 print_endline (string_of_bool test2);;
