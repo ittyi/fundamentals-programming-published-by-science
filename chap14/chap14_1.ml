b6c6838f2a3619b87b7ba4127ceccc08f6409214
ittyi <vioiv.k4@gmail.com>
2025-03-29 22:29:40 +0900

    feat: [ex14_1] 関数 even を List.filter を使って書き直す

--- chap14/chap14_1.ml
+++ chap14/chap14_1.ml
@@ -45,3 +45,20 @@ print_endline (string_of_bool (test1 = false));;
 let test2 = isEven 2
 let () = Printf.printf "test2: ";;
 print_endline (string_of_bool (test2 = true));;
+
+(* 関数 even を List.filter を使って書き直す *)
+let even f lst = List.filter isEven lst
+
+let () = Printf.printf "\n~~let even test ~~\n";;
+let test1 = even isEven [] = []
+let () = Printf.printf "test1: ";;
+print_endline (string_of_bool (test1 = true));;
+let test2 = even isEven [2; 1; 6; 4; 7] = [2; 6; 4]
+let () = Printf.printf "test2: ";;
+print_endline (string_of_bool (test2 = true));;
+let test3 = even isEven [1; 5; 7] = []
+let () = Printf.printf "test3: ";;
+print_endline (string_of_bool (test3 = true));;
+let test4 = even isEven [2; 6; 4] = [2; 6; 4]
+let () = Printf.printf "test4: ";;
+print_endline (string_of_bool (test4 = true));;
