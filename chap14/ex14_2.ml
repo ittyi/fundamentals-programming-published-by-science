ba168a0a245de2afa680a5d9d4a85f0b6ff579c4
ittyi <vioiv.k4@gmail.com>
2025-03-29 23:50:37 +0900

    ✅ test: [ex14_2] count_A のテストを追加

--- chap14/ex14_2.ml
+++ chap14/ex14_2.ml
@@ -42,7 +42,15 @@ let rec count_A lst = match lst with
         else count_A rest
 
 (* test: #use "chap9_6.ml";; *) 
-let test1 = count_A lst1 = 0 
-let test2 = count_A lst2 = 0 
-let test3 = count_A lst3 = 1 
+let test1 = count_A lst1 = 0;;
+let () = Printf.printf "test1: ";;
+print_endline (string_of_bool (test1 = true));;
+let test2 = count_A lst2 = 0
+let () = Printf.printf "test2: ";;
+print_endline (string_of_bool (test2 = true));;
+let test3 = count_A lst3 = 1
+let () = Printf.printf "test3: ";;
+print_endline (string_of_bool (test3 = true));; 
 let test4 = count_A lst4 = 2
+let () = Printf.printf "test4: ";;
+print_endline (string_of_bool (test4 = true));; 
