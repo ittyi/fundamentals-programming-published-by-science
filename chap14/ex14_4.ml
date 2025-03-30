b0b08a7cab9a0499e7293581a6ac2d57d429a171
ittyi <vioiv.k4@gmail.com>
2025-03-30 23:47:30 +0900

    🎨 add: [ex14_4] type gakusei_t を追加

--- chap14/ex14_4.ml
+++ chap14/ex14_4.ml
@@ -1,1 +1,8 @@
 let () = Printf.printf "Hello\n";;
+
+(* 学生ひとり分のデータ（名前、点数、成績）を表す型 *) 
+type gakusei_t = { 
+  namae : string;       (* 名前 *) 
+  tensuu : int;         (* 点数 *) 
+  seiseki : string;     (* 成績 *) 
+} 
