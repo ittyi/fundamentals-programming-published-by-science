d34c7370f2068b1c2ce03f655bebe7b80f18afec
ittyi <vioiv.k4@gmail.com>
2025-03-30 23:50:33 +0900

    📝 add: [ex14_4] 作成する関数の目的、型、命名を作成（total_pointsにした）

--- chap14/ex14_4.ml
+++ chap14/ex14_4.ml
@@ -6,3 +6,7 @@ type gakusei_t = {
   tensuu : int;         (* 点数 *) 
   seiseki : string;     (* 成績 *) 
 } 
+
+(* 目的：gakusei_t 型のリストから、全員の得点合計を返す関数 *)
+(* gakusei_t list -> int *)
+let total_points lst = 0
