fe38b4145ab3f24181c5ca3ddd9df9e8dafc7ff6
ittyi <vioiv.k4@gmail.com>
2025-03-31 23:48:47 +0900

    ✨ feat: [ex14_6] 学生リスト lst のうち、成績が seisekiXXX の人の数を返す関数 count を filter と length を使って解いた。

--- chap14/ex14_6.ml
+++ chap14/ex14_6.ml
@@ -2,7 +2,11 @@ let () = Printf.printf "Hello\n";;
 
 (* 目的：学生リスト lst のうち、成績が seisekiXXX の人の数を返す *)
 (* a` list -> b` -> int *)
-let count lst target = 0
+(* お題：filter と length を使って解く。 *)
+let count lst target = 
+  let isSame target = target = "0" in
+  let tmp = List.filter isSame lst in
+  List.length tmp
 
 let () = Printf.printf "\n~~let count test ~~\n";;
 let test1 = count [] 0 = 0
@@ -17,10 +21,6 @@ let test3 = count ["0"; "0"] "0" = 2
 let () = Printf.printf "test3: ";;
 print_endline (string_of_bool (test3 = true));;
 
-let test4 = count [0; 0] "0" = 0
+let test4 = count ["0"; "1"] "0" = 1
 let () = Printf.printf "test4: ";;
 print_endline (string_of_bool (test4 = true));;
-
-let test5 = count ["0"; "1"] "0" = 1
-let () = Printf.printf "test5: ";;
-print_endline (string_of_bool (test5 = true));;
