36a5fbeaccd5e018e0f2eb4873dc6fae108accd7
ittyi <vioiv.k4@gmail.com>
2025-03-31 23:40:36 +0900

    ✅ test: [ex14_6] green test を追加

--- chap14/ex14_6.ml
+++ chap14/ex14_6.ml
@@ -3,3 +3,8 @@ let () = Printf.printf "Hello\n";;
 (* 目的：学生リスト lst のうち、成績が seisekiXXX の人の数を返す *)
 (* a` list -> b` -> int *)
 let count lst target = 0
+
+let () = Printf.printf "\n~~let count test ~~\n";;
+let test1 = count [] 0 = 0
+let () = Printf.printf "test1: ";;
+print_endline (string_of_bool (test1 = true));;
