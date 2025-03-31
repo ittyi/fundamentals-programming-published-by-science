946c9209c4fc8841f3d1f3e68828d5b960648858
ittyi <vioiv.k4@gmail.com>
2025-03-31 23:26:49 +0900

    ✨ feat: [ex14_5] 偶数の数を数える関数 even を局所関数で作成。all green

--- chap14/ex14_5.ml
+++ chap14/ex14_5.ml
@@ -1,28 +1,20 @@
 let () = Printf.printf "Hello\n";;
 
-let isEven n = n mod 2 == 0
-
-let () = Printf.printf "\n~~let isEven test ~~\n";;
-let test1 = isEven 1
-let () = Printf.printf "test1: ";;
-print_endline (string_of_bool (test1 = false));;
-let test2 = isEven 2
-let () = Printf.printf "test2: ";;
-print_endline (string_of_bool (test2 = true));;
-
-(* 関数 even を List.filter を使って書き直す *)
-let even f lst = List.filter isEven lst
+(* 偶数の数を数える関数 *)
+let even lst = 
+  let isEven n = n mod 2 == 0 in
+  List.filter isEven lst
 
 let () = Printf.printf "\n~~let even test ~~\n";;
-let test1 = even isEven [] = []
+let test1 = even [] = []
 let () = Printf.printf "test1: ";;
 print_endline (string_of_bool (test1 = true));;
-let test2 = even isEven [2; 1; 6; 4; 7] = [2; 6; 4]
+let test2 = even [2; 1; 6; 4; 7] = [2; 6; 4]
 let () = Printf.printf "test2: ";;
 print_endline (string_of_bool (test2 = true));;
-let test3 = even isEven [1; 5; 7] = []
+let test3 = even [1; 5; 7] = []
 let () = Printf.printf "test3: ";;
 print_endline (string_of_bool (test3 = true));;
-let test4 = even isEven [2; 6; 4] = [2; 6; 4]
+let test4 = even [2; 6; 4] = [2; 6; 4]
 let () = Printf.printf "test4: ";;
 print_endline (string_of_bool (test4 = true));;
