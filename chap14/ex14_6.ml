e0cec05b36db38a8dc83ab39f443b4c7edc9e2a3
ittyi <vioiv.k4@gmail.com>
2025-03-31 23:43:04 +0900

    ✅ test: [ex14_6] 関数 count の red test を必要十分に追加

--- chap14/ex14_6.ml
+++ chap14/ex14_6.ml
@@ -8,3 +8,19 @@ let () = Printf.printf "\n~~let count test ~~\n";;
 let test1 = count [] 0 = 0
 let () = Printf.printf "test1: ";;
 print_endline (string_of_bool (test1 = true));;
+
+let test2 = count ["0"] "0" = 1
+let () = Printf.printf "test2: ";;
+print_endline (string_of_bool (test2 = true));;
+
+let test3 = count ["0"; "0"] "0" = 2
+let () = Printf.printf "test3: ";;
+print_endline (string_of_bool (test3 = true));;
+
+let test4 = count [0; 0] "0" = 0
+let () = Printf.printf "test4: ";;
+print_endline (string_of_bool (test4 = true));;
+
+let test5 = count ["0"; "1"] "0" = 1
+let () = Printf.printf "test5: ";;
+print_endline (string_of_bool (test5 = true));;
