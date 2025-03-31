f1675d1d48fb77599b64f77f307a1e67d500bb69
ittyi <vioiv.k4@gmail.com>
2025-03-31 23:33:12 +0900

    ✨ feat: [ex14_5] gakusei_t 型のリストから、全員の得点合計を返す関数 total_points を局所関数で作成

--- chap14/ex14_5.ml
+++ chap14/ex14_5.ml
@@ -67,3 +67,43 @@ print_endline (string_of_bool (test1 = true));;
 let test2 = concat ["春"; "夏"; "秋"; "冬"] = "春夏秋冬"
 let () = Printf.printf "test2: ";;
 print_endline (string_of_bool (test2 = true));;
+
+
+(* 目的：gakusei_t 型のリストから、全員の得点合計を返す関数 *)
+(* gakusei_t list -> int *)
+let total_points lst = 
+  let add_point first rest_result = first.tensuu + rest_result in
+  List.fold_right add_point lst 0
+
+let () = Printf.printf "\n~~ let total_points test ~~ \n";;
+let test1 = total_points [] = 0
+let () = Printf.printf "test1: ";;
+print_endline (string_of_bool (test1 = true));;
+
+let test2 = total_points [{ 
+  namae = "";
+  tensuu = 0;
+  seiseki = ""; 
+}] = 0
+let () = Printf.printf "test2: ";;
+print_endline (string_of_bool (test2 = true));;
+
+let test3 = total_points [{ 
+  namae = "";
+  tensuu = 1;
+  seiseki = ""; 
+}] = 1
+let () = Printf.printf "test3: ";;
+print_endline (string_of_bool (test3 = true));;
+
+let test4 = total_points [{ 
+  namae = "";
+  tensuu = 1;
+  seiseki = ""; 
+};{ 
+  namae = "";
+  tensuu = 100;
+  seiseki = ""; 
+}] = 101
+let () = Printf.printf "test4: ";;
+print_endline (string_of_bool (test4 = true));;
