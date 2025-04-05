dc9b3e1b3e2c8777574ae02076651705e27d3c0d
ittyi <vioiv.k4@gmail.com>
2025-04-05 23:29:05 +0900

    ✅ ✨ feat: 名前のない関数 を add1 という変数に格納してみる

--- chap14/ch14_4.ml
+++ chap14/ch14_4.ml
@@ -8,3 +8,13 @@ print_endline (string_of_bool (test1 = true));;
 let test2 = (fun x -> x + 1) 2 = 3
 let () = Printf.printf "test2: ";;
 print_endline (string_of_bool (test2 = true));;
+
+let add1 = fun x -> x + 1;;
+let () = Printf.printf "\n~~add1 test ~~\n";;
+let test1 = add1 1 = 2
+let () = Printf.printf "test1: ";;
+print_endline (string_of_bool (test1 = true));;
+
+let test2 = add1 2 = 3
+let () = Printf.printf "test2: ";;
+print_endline (string_of_bool (test2 = true));;
