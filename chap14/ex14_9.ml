d7d18d57a87ff916f84d9fa3353944463692962b
ittyi <vioiv.k4@gmail.com>
2025-04-06 00:01:43 +0900

    ✅ test: [ex14-9] fun parson -> parson.name のテストを作成

--- chap14/ex14_9.ml
+++ chap14/ex14_9.ml
@@ -9,3 +9,24 @@ type parson_t = {
 }
 
 (* fun parson -> parson.name;; *)
+let () = Printf.printf "\n~~fun parson -> parson.name test ~~\n";;
+let test1 = (fun parson -> parson.name) {
+  name= "string";
+  height= 1;
+  body_weight= 1;
+  birthday= "string";
+  blood_type= "string";
+} = "string"
+let () = Printf.printf "test1: ";;
+print_endline (string_of_bool (test1 = true));;
+
+let test2 = (fun parson -> parson.name) {
+  name= "string2";
+  height= 1;
+  body_weight= 1;
+  birthday= "string";
+  blood_type= "string";
+} = "string2"
+let () = Printf.printf "test2: ";;
+print_endline (string_of_bool (test2 = true));;
+
