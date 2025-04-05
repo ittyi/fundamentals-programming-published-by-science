5508734ac1b0ecb28d896508374c8e04cd0ab8d0
ittyi <vioiv.k4@gmail.com>
2025-04-05 23:34:38 +0900

    ✨ feat: 受け取ったリスト lst の各要素の和を求める関数 sum を名前なし関数を使用して作成

--- chap14/ch14_4.ml
+++ chap14/ch14_4.ml
@@ -21,3 +21,6 @@ print_endline (string_of_bool (test2 = true));;
 
 (* 目的：受け取ったリスト lst の各要素の和を求める *)
 (* sum : int list -> int *)
+let sum = 
+  fun lst -> let add_int first rest_result = first + rest_result in
+  List.fold_right add_int lst 0;;
