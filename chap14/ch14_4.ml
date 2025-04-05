3a8b413b649538edd63104471962e32ff56ebde8
ittyi <vioiv.k4@gmail.com>
2025-04-05 23:36:07 +0900

    ✅ feat: [ch14_4] sum のテスト追加

--- chap14/ch14_4.ml
+++ chap14/ch14_4.ml
@@ -24,3 +24,20 @@ print_endline (string_of_bool (test2 = true));;
 let sum = 
   fun lst -> let add_int first rest_result = first + rest_result in
   List.fold_right add_int lst 0;;
+
+let () = Printf.printf "\n~~sum test ~~\n";;
+let test1 = sum [] = 0
+let () = Printf.printf "test1: ";;
+print_endline (string_of_bool (test1 = true));;
+
+let test2 = sum [1] = 1
+let () = Printf.printf "test2: ";;
+print_endline (string_of_bool (test2 = true));;
+
+let test3 = sum [1; 1] = 2
+let () = Printf.printf "test3: ";;
+print_endline (string_of_bool (test3 = true));;
+
+let test4 = sum [1; 2] = 3
+let () = Printf.printf "test4: ";;
+print_endline (string_of_bool (test4 = true));;
