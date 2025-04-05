b476a9c339e3a787bd3a6713c2a7087e391530e0
ittyi <vioiv.k4@gmail.com>
2025-04-05 23:31:15 +0900

    📝 add: 受け取ったリスト lst の各要素の和を求める関数 sum の目的と型定義を記載

--- chap14/ch14_4.ml
+++ chap14/ch14_4.ml
@@ -18,3 +18,6 @@ print_endline (string_of_bool (test1 = true));;
 let test2 = add1 2 = 3
 let () = Printf.printf "test2: ";;
 print_endline (string_of_bool (test2 = true));;
+
+(* 目的：受け取ったリスト lst の各要素の和を求める *)
+(* sum : int list -> int *)
