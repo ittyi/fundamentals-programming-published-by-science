0860f60321a09c84d8fef9e183255f48a649080f
ittyi <vioiv.k4@gmail.com>
2025-04-07 00:39:53 +0900

    ✨ feat: [ex14_11] let shokika を map と名前なし関数を使って書き直す

--- chap14/ex14_11.ml
+++ chap14/ex14_11.ml
@@ -71,30 +71,30 @@ let eki_list = [
 {namae="本郷三丁目"; saitan_kyori = infinity; temae_list = []}; 
 {namae="御茶ノ水"; saitan_kyori = infinity; temae_list = []} 
 ] 
- 
+
 (* 目的：ekimei list から eki list を作る *) 
 (* shokika : eki_t list -> string -> eki_t list *) 
-let rec shokika eki_list kiten = match eki_list with 
-    [] -> [] 
-  | ({namae = k; saitan_kyori = s; temae_list = t} as first) :: rest -> 
-      if k = kiten 
-      then {namae = k; saitan_kyori = 0.; temae_list = [k]} 
-	   :: shokika rest kiten 
-      else first :: shokika rest kiten 
- 
+let shokika eki_list kiten = 
+    List.map (fun eki -> 
+        if eki.namae = kiten 
+            then {namae = eki.namae; saitan_kyori = 0.; temae_list = [eki.namae]}
+            else eki
+    ) eki_list
+
 (* テスト *) 
-let () = Printf.printf "\n~~ make_eki_list test ~~ \n";;
-let test1 = shokika [] "茗荷谷" = [] 
+let () = Printf.printf "\n~~ make_eki_list test ~~ \n"
+let test1 = shokika [] "茗荷谷" = []
 let () = Printf.printf "test1: ";;
 print_endline (string_of_bool (test1 = true));;
 
-let test2 = shokika eki_list "茗荷谷" = [ 
+let test2 = shokika eki_list "茗荷谷" = [
 {namae="池袋"; saitan_kyori = infinity; temae_list = []}; 
 {namae="新大塚"; saitan_kyori = infinity; temae_list = []}; 
 {namae="茗荷谷"; saitan_kyori = 0.; temae_list = ["茗荷谷"]}; 
 {namae="後楽園"; saitan_kyori = infinity; temae_list = []}; 
 {namae="本郷三丁目"; saitan_kyori = infinity; temae_list = []}; 
-{namae="御茶ノ水"; saitan_kyori = infinity; temae_list = []} 
-] 
+{namae="御茶ノ水"; saitan_kyori = infinity; temae_list = []};
+]
+
 let () = Printf.printf "test2: ";;
 print_endline (string_of_bool (test2 = true));;
