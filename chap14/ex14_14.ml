9a442bcdc1e54a4b7c57299639ccf8aeb25d8a28
ittyi <vioiv.k4@gmail.com>
2025-04-16 01:32:01 +0900

    ✨ feat: [ex14_14] 関数 concat を prefix 関数で実現

--- chap14/ex14_14.ml
+++ chap14/ex14_14.ml
@@ -1,1 +1,11 @@
 let () = Printf.printf "Hello\n";;
+
+let concat lst = List.fold_right (^) lst "";;
+
+let () = Printf.printf "\n~~ let concat test ~~ \n";;
+let test1 = concat [] = ""
+let () = Printf.printf "test1: ";;
+print_endline (string_of_bool (test1 = true));;
+let test2 = concat ["春"; "夏"; "秋"; "冬"] = "春夏秋冬"
+let () = Printf.printf "test2: ";;
+print_endline (string_of_bool (test2 = true));;
