81607e052894df24582d30c659abf00987f2f2d6
ittyi <vioiv.k4@gmail.com>
2025-05-02 23:47:05 +0900

    ✨ feat: [ch15_4] 総数の第 n 項の値を求める関数 dai_n_kou を作成

--- chap15/ch15_4.ml
+++ chap15/ch15_4.ml
@@ -1,1 +1,9 @@
 let () = Printf.printf "hello\n";;
+
+(* 目的：総数の第 n 項の値を求める *)
+(* dai_n_kou : int -> float *)
+let rec dai_n_kou n = 
+    if n = 0
+        then 1.0
+        else dai_n_kou (n - 1) /. float_of_int n 
+
