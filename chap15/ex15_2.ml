54eb94c57e6fefa5762bf3f70b6a8436a4002376
ittyi <vioiv.k4@gmail.com>
2025-05-31 23:42:51 +0900

    ✅ test: let gcd の green, red test を作成

--- chap15/ex15_2.ml
+++ chap15/ex15_2.ml
@@ -6,3 +6,13 @@ let gcd m n =
   if n = 0
     then  0
     else  0
+
+(* テスト *) 
+let () = Printf.printf "\n~~ let gcd test ~~ \n"
+let test1 = gcd 0 0 = 0
+let () = Printf.printf "test1: ";;
+print_endline (string_of_bool (test1 = true));;
+
+let test2 = gcd 1 0 = 1
+let () = Printf.printf "test2: ";;
+print_endline (string_of_bool (test2 = true));;
