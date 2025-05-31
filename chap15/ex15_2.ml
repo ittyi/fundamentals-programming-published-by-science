144a010955af9dddf93d66e0e5af9619d484cafd
ittyi <vioiv.k4@gmail.com>
2025-05-31 23:46:43 +0900

    ✅ test: let gcd の自明ではないケースの red test を追加

--- chap15/ex15_2.ml
+++ chap15/ex15_2.ml
@@ -16,3 +16,11 @@ print_endline (string_of_bool (test1 = true));;
 let test2 = gcd 1 0 = 1
 let () = Printf.printf "test2: ";;
 print_endline (string_of_bool (test2 = true));;
+
+let test3 = gcd 2 1 = 2
+let () = Printf.printf "test3: ";;
+print_endline (string_of_bool (test3 = true));;
+
+let test4 = gcd 8177 3315 = 221
+let () = Printf.printf "test4: ";;
+print_endline (string_of_bool (test4 = true));;
