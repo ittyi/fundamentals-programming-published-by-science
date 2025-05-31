9c4b3cbbac18cf1683a156071386286974b1118b
ittyi <vioiv.k4@gmail.com>
2025-05-31 23:41:24 +0900

    🚧 add: デザインレシピに従い、まずは自明なケースの if 文だけ作成

--- chap15/ex15_2.ml
+++ chap15/ex15_2.ml
@@ -2,4 +2,7 @@ let () = Printf.printf "hello\n";;
 
 (* 目的：二つの自然数 m と n の最大公約数を求める関数 gcd をユークリッドの互除法を使って書く *)
 (* int -> int -> float *)
-let gcd m n = 0.0
+let gcd m n =
+  if n = 0
+    then  0
+    else  0
